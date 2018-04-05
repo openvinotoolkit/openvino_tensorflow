baa87743cadd683f3a6fbb4d1e159f421e275d97
Avijit <30507445+avijit-nervana@users.noreply.github.com>
2018-04-05 15:07:14 -0700

    Avijit/installer fix (#36)
    * Added support to copy ngraph artifacts to the installation directory.
    * Added auto detection of CXXABI flag from TensorFlow.
    * Refactored the TensorFlow detection/setting to a FindTensorFlow.cmake
    that will support calling find_package for TensorFlow.
    * Added support for removing the installer created directories.

--- cmake/cmake_uninstall.in.cmake
+++ cmake/cmake_uninstall.in.cmake
@@ -19,16 +19,18 @@ file(READ "@CMAKE_BINARY_DIR@/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 foreach(file ${files})
   message(STATUS "Uninstalling $ENV{DESTDIR}${file}")
+  get_filename_component( DIR ${file} DIRECTORY )
+  list(APPEND DoomedDirList ${DIR})
   if(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
-    exec_program(
-      "@CMAKE_COMMAND@" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
-      OUTPUT_VARIABLE rm_out
-      RETURN_VALUE rm_retval
-      )
-    if(NOT "${rm_retval}" STREQUAL 0)
-      message(FATAL_ERROR "Problem when removing $ENV{DESTDIR}${file}")
-    endif(NOT "${rm_retval}" STREQUAL 0)
-  else(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+    file( REMOVE_RECURSE ${file})  
+  else()
     message(STATUS "File $ENV{DESTDIR}${file} does not exist.")
-  endif(IS_SYMLINK "$ENV{DESTDIR}${file}" OR EXISTS "$ENV{DESTDIR}${file}")
+  endif()
 endforeach(file)
+
+# Now remove the directories
+list(REMOVE_DUPLICATES DoomedDirList)
+foreach( dir ${DoomedDirList})
+  message( STATUS "Removing directory: " ${dir} )
+  file(REMOVE_RECURSE ${dir})
+endforeach()
