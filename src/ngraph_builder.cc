7f8a723307a189456fcab7ede6fb9ddd419a451d
Avijit <Avijit.Chakraborty@intel.com>
2018-05-06 14:42:26 -0700

    Added the nGraph library dependency, started to add a builder which will convert the TF graph to nGrah function.

---
+++ src/ngraph_builder.cc
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright 2017-2018 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+#include "ngraph_builder.h"
+
+#include "tensorflow/core/framework/tensor.pb.h"
+#include "tensorflow/core/framework/tensor_shape.pb.h"
+#include "tensorflow/core/framework/tensor_shape.pb_text.h"
+#include "tensorflow/core/graph/algorithm.h"
+
+namespace ngraph_bridge {
+unique_ptr<ng::Function> Builder::TransformGraph(const tf::Graph* input_graph) {
+  //   for (const tf::Node* n : input_graph->nodes()) {
+  //     cout << "Node: " << n->name() << " Type: " << n->type_string() << endl;
+  //     for (const tf::Edge* edge : n->in_edges()) {
+  //       cout << "\tEdge " << edge->src()->name() << " --> " <<
+  //       edge->dst()->name()
+  //            << endl;
+  //     }
+  //   }
+  // Do a topological sort
+  vector<tf::Node*> ordered;
+  tf::GetReversePostOrder(*input_graph,
+                          &ordered);  // This will give us topological sort.
+
+  cout << "After Topological sort\n";
+  //   for (auto n : ordered) {
+  //     cout << "Node: " << n->name() << " Type: " << n->type_string() << endl;
+  //     for (const tf::Edge* edge : n->in_edges()) {
+  //       cout << "\tEdge " << edge->src()->name() << " --> " <<
+  //       edge->dst()->name()
+  //            << endl;
+  //     }
+  //   }
+
+  // Now start building the nGraph.
+  unordered_map<string, shared_ptr<ng::op::Parameter>> parameter_map;
+  for (auto n : ordered) {
+    tf::AttrSlice n_attrs = n->attrs();
+    if (n->type_string() == "Placeholder") {
+      tf::DataType dtype;
+      if (tf::GetNodeAttr(n_attrs, "dtype", &dtype) != tf::Status::OK()) {
+        cout << "Error: No data type defined for Parameter!\n";
+        return nullptr;
+      }
+      tf::TensorShapeProto shape_proto;
+      if (tf::GetNodeAttr(n_attrs, "shape", &shape_proto) != tf::Status::OK()) {
+        cout << "Error: No shape type defined for Parameter!\n";
+        return nullptr;
+      }
+
+      // Get the name of the node
+      cout << "Parameter: " << n->name() << "[" << tf::DataTypeString(dtype) << "] Shape: ";
+      tf::PartialTensorShape shape(shape_proto);
+
+      for (int d = 0; d < shape.dims(); ++d) {
+        if (d > 0) cout << ", ";
+        cout << shape.dim_size(d);
+      }
+
+      // Create the nGraph::op::Parameter
+      // TODO
+      //<< " Shape: " << tf::ProtoShortDebugString(shape) << endl;
+      cout << endl;
+    } else {
+      // cout << "Node: " << n->name() << " Type: " << n->type_string() << endl;
+      for (const tf::Edge* edge : n->in_edges()) {
+        cout << "\tEdge " << edge->src()->name() << " --> "
+             << edge->dst()->name() << endl;
+      }
+    }
+  }
+
+  return nullptr;
+}
+
+}  // namespace ngraph_bridge
--- test/graph_exec.cpp
+++ test/graph_exec.cpp
@@ -15,10 +15,12 @@
  *******************************************************************************/
 #include "gtest/gtest.h"
 
+#include "ngraph_builder.h"
 #include "ngraph_utils.h"
 
 #include "tensorflow/core/framework/graph.pb.h"
 #include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/graph/algorithm.h"
 #include "tensorflow/core/graph/graph.h"
 #include "tensorflow/core/graph/graph_constructor.h"
 #include "tensorflow/core/platform/env.h"
@@ -26,9 +28,19 @@
 using namespace std;
 namespace tf = tensorflow;
 
-TEST(graph_exec, simple) {
-  // tf::Graph graph(tf::OpRegistry::Global());
+TEST(graph_exec, builder) {
+  tf::GraphDef gdef;
+  auto status = tf::ReadTextProto(tf::Env::Default(), "test_py.pbtxt", &gdef);
+  ASSERT_TRUE(status == tf::Status::OK()) << "Can't read protobuf graph";
 
+  tf::Graph input_graph(tf::OpRegistry::Global());
+  tf::GraphConstructorOptions opts;
+  ASSERT_EQ(tf::ConvertGraphDefToGraph(opts, gdef, &input_graph),
+            tf::Status::OK());
+  auto ng_function = ngraph_bridge::Builder::TransformGraph(&input_graph);
+}
+
+TEST(graph_exec, DISABLED_simple) {
   tf::GraphDef gdef;
   auto status = tf::ReadTextProto(tf::Env::Default(), "test_py.pbtxt", &gdef);
   EXPECT_TRUE(status == tf::Status::OK()) << "Can't read protobuf graph";
@@ -37,6 +49,29 @@ TEST(graph_exec, simple) {
   tf::GraphConstructorOptions opts;
   ASSERT_EQ(tf::ConvertGraphDefToGraph(opts, gdef, &input_graph),
             tf::Status::OK());
+
+  for (const tf::Node* n : input_graph.nodes()) {
+    cout << "Node: " << n->name() << " Type: " << n->type_string() << endl;
+    for (const tf::Edge* edge : n->in_edges()) {
+      cout << "\tEdge " << edge->src()->name() << " --> " << edge->dst()->name()
+           << endl;
+    }
+  }
+  // Do a topological sort
+  vector<tf::Node*> ordered;
+  tf::GetReversePostOrder(input_graph,
+                          &ordered);  // This will give us topological sort.
+
+  cout << "After Topo sort\n";
+  for (auto n : ordered) {
+    cout << "Node: " << n->name() << " Type: " << n->type_string() << endl;
+    for (const tf::Edge* edge : n->in_edges()) {
+      cout << "\tEdge " << edge->src()->name() << " --> " << edge->dst()->name()
+           << endl;
+    }
+  }
+
+  // Next create the nGraph graph
 
   GraphToPbTextFile("./test_graph.pbtxt", &input_graph);
 
