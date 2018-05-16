2d63c02668611dbfb9539a33ffe2797f50e01229
Avijit <Avijit.Chakraborty@intel.com>
2018-05-15 17:06:28 -0700

    Merged with Adam's code. Also updated the test_axpy.py to use NGRAPH_CPU.

--- test/python/test_axpy.py
+++ test/python/test_axpy.py
@@ -50,7 +50,7 @@ a = np.full((2, 3), 5.0)
 x = tf.placeholder(tf.float32, [None, 3], name='x')
 y = tf.placeholder(tf.float32, shape=(2, 3), name='y')
 
-with tf.device("/device:CPU:0"):
+with tf.device("/device:NGRAPH_CPU:0"):
     #with tf.device("/device:XLA_CPU:0"):
     c = a * x
     axpy = c + y
