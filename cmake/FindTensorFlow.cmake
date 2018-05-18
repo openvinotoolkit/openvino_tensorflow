044775e70a3298d76d3f8ea8b8fdbc67920b72c5
Avijit <Avijit.Chakraborty@intel.com>
2018-05-18 16:21:58 -0700

    Moved cmake for repo-move

---
+++ cmake/FindTensorFlow.cmake
@@ -0,0 +1,108 @@
+# ******************************************************************************
+# Copyright 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ******************************************************************************
+
+# Try to find TensorFlow library and include paths
+#
+# The following are set after configuration is done:
+#  TensorFlow_FOUND
+#  TensorFlow_INCLUDE_DIR
+#  TensorFlow_LIBRARIES
+#  TensorFlow_LIBRARY_DIRS
+#  TensorFlow_DIR
+
+include(FindPackageHandleStandardArgs)
+message( STATUS "Looking for TensorFlow installation" )
+
+execute_process(
+    COMMAND 
+    python -c "import tensorflow as tf; print(tf.sysconfig.get_include())"
+    RESULT_VARIABLE result
+    OUTPUT_VARIABLE TEMP
+    ERROR_VARIABLE ERR
+)
+
+if( NOT ${result} )
+  string(REGEX REPLACE "\n$" "" TensorFlow_INCLUDE_DIR "${TEMP}")
+else()
+   message( FATAL_ERROR "Cannot determine TensorFlow installation directory " ${ERR} )
+endif()
+MESSAGE( STATUS "TensorFlow_INCLUDE_DIR: " ${TensorFlow_INCLUDE_DIR} )
+
+execute_process(
+    COMMAND 
+    python -c "import tensorflow as tf; print(tf.sysconfig.get_lib())"
+    RESULT_VARIABLE result
+    OUTPUT_VARIABLE TEMP
+    ERROR_VARIABLE ERR
+)
+if( NOT ${result} )
+  string(REGEX REPLACE "\n$" "" TensorFlow_DIR "${TEMP}")
+else()
+   message( FATAL_ERROR "Cannot determine TensorFlow installation directory\n" ${ERR} )
+endif()
+message( STATUS "TensorFlow_DIR: " ${TensorFlow_DIR} )
+
+execute_process(
+    COMMAND 
+    python -c "import tensorflow as tf; print(tf.__cxx11_abi_flag__)"
+    RESULT_VARIABLE result
+    OUTPUT_VARIABLE TEMP
+    ERROR_VARIABLE ERR
+)
+if( NOT ${result} )
+  string(REGEX REPLACE "\n$" "" TensorFlow_CXX_ABI "${TEMP}")
+else()
+   message( FATAL_ERROR "Cannot determine TensorFlow __cxx11_abi_flag__\n" ${ERR} )
+endif()
+message( STATUS "TensorFlow_CXX_ABI: " ${TensorFlow_CXX_ABI} )
+
+execute_process(
+    COMMAND 
+    python -c "import tensorflow as tf; print(tf.__git_version__)"
+    RESULT_VARIABLE result
+    OUTPUT_VARIABLE TEMP
+    ERROR_VARIABLE ERR
+)
+if( NOT ${result} )
+  string(REGEX REPLACE "\n$" "" TensorFlow_GIT_VERSION "${TEMP}")
+else()
+   message( FATAL_ERROR "Cannot determine TensorFlow __git_version__\n" ${ERR} )
+endif()
+message( STATUS "TensorFlow_GIT_VERSION: " ${TensorFlow_GIT_VERSION} )
+
+# Make sure that the TF library exists
+find_library(
+  TensorFlow_FRAMEWORK_LIBRARY
+  NAME tensorflow_framework
+  PATHS ${TensorFlow_DIR}
+  NO_DEFAULT_PATH
+)
+
+find_package_handle_standard_args(
+  TensorFlow 
+  FOUND_VAR TensorFlow_FOUND   
+  REQUIRED_VARS 
+    TensorFlow_DIR  
+    TensorFlow_INCLUDE_DIR   
+    TensorFlow_CXX_ABI  
+    TensorFlow_GIT_VERSION  
+    TensorFlow_FRAMEWORK_LIBRARY
+)
+
+# if(CUDNN_FOUND)
+#   message(STATUS "Found cuDNN: v${CUDNN_VERSION}  (include: ${CUDNN_INCLUDE_DIR}, library: ${CUDNN_LIBRARY})")
+#   mark_as_advanced(CUDNN_ROOT_DIR CUDNN_LIBRARY CUDNN_INCLUDE_DIR)
+# endif()
---
+++ cmake/external_gtest.cmake
@@ -0,0 +1,81 @@
+# ******************************************************************************
+# Copyright 2017-2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ******************************************************************************
+
+# Enable ExternalProject CMake module
+include(ExternalProject)
+
+#----------------------------------------------------------------------------------------------------------
+# Download and install GoogleTest ...
+#----------------------------------------------------------------------------------------------------------
+
+SET(GTEST_GIT_REPO_URL https://github.com/google/googletest.git)
+SET(GTEST_GIT_LABEL release-1.8.0)
+
+# The 'BUILD_BYPRODUCTS' argument was introduced in CMake 3.2.
+if (${CMAKE_VERSION} VERSION_LESS 3.2)
+    ExternalProject_Add(
+        ext_gtest
+        GIT_REPOSITORY ${GTEST_GIT_REPO_URL}
+        GIT_TAG ${GTEST_GIT_LABEL}
+        # Disable install step
+        INSTALL_COMMAND ""
+        UPDATE_COMMAND ""
+        #CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_FLAGS="-fPIC"
+        CMAKE_ARGS -DCMAKE_CXX_FLAGS="-fPIC"
+        TMP_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/tmp"
+        STAMP_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/stamp"
+        DOWNLOAD_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/download"
+        SOURCE_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/src"
+        BINARY_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/build"
+        INSTALL_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest"
+        )
+else()
+    ExternalProject_Add(
+        ext_gtest
+        GIT_REPOSITORY ${GTEST_GIT_REPO_URL}
+        GIT_TAG ${GTEST_GIT_LABEL}
+        # Disable install step
+        INSTALL_COMMAND ""
+        UPDATE_COMMAND ""
+        #CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_FLAGS="-fPIC"
+        CMAKE_ARGS -DCMAKE_CXX_FLAGS="-fPIC"
+        TMP_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/tmp"
+        STAMP_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/stamp"
+        DOWNLOAD_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/download"
+        SOURCE_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/src"
+        BINARY_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/build"
+        INSTALL_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest"
+        BUILD_BYPRODUCTS "${EXTERNAL_PROJECTS_ROOT}/gtest/build/googlemock/gtest/libgtest.a"
+        )
+endif()
+
+#----------------------------------------------------------------------------------------------------------
+
+get_filename_component(
+    GTEST_INCLUDE_DIR
+    "${EXTERNAL_PROJECTS_ROOT}/gtest/src/googletest/include"
+    ABSOLUTE)
+set(GTEST_INCLUDE_DIR "${GTEST_INCLUDE_DIR}" PARENT_SCOPE)
+
+# Create a libgtest target to be used as a dependency by test programs
+add_library(libgtest IMPORTED STATIC GLOBAL)
+add_dependencies(libgtest ext_gtest)
+
+# Set libgtest properties
+set_target_properties(libgtest PROPERTIES
+    "IMPORTED_LOCATION" "${EXTERNAL_PROJECTS_ROOT}/gtest/build/googlemock/gtest/libgtest.a"
+    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
+)
