dd6c5420d68f5ff779c0e268b7772aea93ec2996
Adam Procter <adam.m.procter@intel.com>
2018-06-07 21:56:07 -0700

    Fix ValuesFromConstNode for the non-repeated int_val/float_val case

--- src/ngraph_utils.h
+++ src/ngraph_utils.h
@@ -96,10 +96,10 @@ tf::Status ValuesFromConstNode(const tf::NodeDef& node,
       switch (node.attr().at("dtype").type()) {
         // TODO(amprocte): there are more element types to support here
         case tf::DT_INT32:
-          values->data()[i] = tensor.int_val()[i];
+          values->data()[i] = (tensor.int_val_size() == 1 ? tensor.int_val()[0] : tensor.int_val()[i]);
           break;
         case tf::DT_FLOAT:
-          values->data()[i] = tensor.float_val()[i];
+          values->data()[i] = (tensor.float_val_size() == 1 ? tensor.float_val()[0] : tensor.float_val()[i]);
           break;
         default:
           NGRAPH_VLOG(0)
