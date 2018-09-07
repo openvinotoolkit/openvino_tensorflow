e5956045e1e3dd2fbe57d48e93390c1ac20e365d
crlishka <30601430+crlishka@users.noreply.github.com>
2018-09-06 22:21:24 -0700

    Support for JUnit XML report generation of unit-tests (#182)
    * Add clang-3.9-format for style-checks in Python 3 builds.  Add comment for why clang-3.9-format is being included.
    
    * Generate JUnit XML file in build directory for now.  Probably need to make the XML filename more unique.
    
    * Fill out the JUnit XML reporting by porting shell-script additions for JUnit from ngraph-tensorflow-1.3.  Write all XML scripts to the top-level of the ngraph-tf repo.
    
    * Put the location of the xml files in the BUILD_DIR, since that directory is ephemeral and easy to clean by removing it.  Plus, the unit-testing XMLs are associated with the testing for a particular build, so BUILD_DIR is the logical place.

---
+++ test/ci/junit-wrap.sh
@@ -0,0 +1,143 @@
+#!  /bin/bash
+
+# This script is a wrapper which captures the output of a script and writes
+# a proper JUnit XML file, for consumption by Jenkins.  This was adapated
+# from run-integration-tests.sh at path
+# tensorflow/compiler/plugin/ngraph/tests/integration-tests in repo
+# NervanaSystems/ngraph-tensorflow-1.3
+
+# Before calling this script, you should set three environment variables:
+#
+#   JUNIT_WRAP_FILE   Name of the file and path to write XML output to
+#   JUNIT_WRAP_SUITE  Name of the JUnit "suite" to record results for
+#   JUNIT_WRAP_TEST   Name of the JUnit "test" to record results for
+#
+# Note that only one test is run in the suite, with the test having a
+# pass/fail status based on the exit code of the command
+
+# -----------------------------------------------------------------------------
+# Globals
+# -----------------------------------------------------------------------------
+
+declare JUNIT_XML=''  # XML for testsuites and testcases
+declare -i JUNIT_SUITES=0
+declare -i JUNIT_TESTS=0
+declare -i JUNIT_PASS=0
+declare -i JUNIT_FAIL=0
+declare -i JUNIT_DISABLED=0
+declare JUNIT_SUITE_NAME='UNDEFINED_SUITE'
+declare JUNIT_SUITE_XML=''  # XML for testcases
+declare -i JUNIT_SUITE_TESTS=0
+declare -i JUNIT_SUITE_PASS=0
+declare -i JUNIT_SUITE_FAIL=0
+declare -i JUNIT_SUITE_DISABLED=0
+declare NL=$'\n'  # Used to insert newlines in double-quoted strings
+
+# -----------------------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------------------
+
+xml_start_suite() {
+
+    JUNIT_SUITE_NAME="${JUNIT_WRAP_SUITE}"
+    JUNIT_SUITE_TESTS=0
+    JUNIT_SUITE_PASS=0
+    JUNIT_SUITE_FAIL=0
+    JUNIT_SUITE_DISABLED=0
+
+    JUNIT_SUITE_XML=''
+    (( JUNIT_SUITES += 1))
+}
+
+xml_finish_suite() {
+
+    JUNIT_XML="${JUNIT_XML}${NL}  <testsuite name=\"${JUNIT_SUITE_NAME}\" tests=\"${JUNIT_SUITE_TESTS}\" failures=\"${JUNIT_SUITE_FAIL}\" disabled=\"${JUNIT_SUITE_DISABLED}\" error=\"0\" time=\"0\">  ${JUNIT_SUITE_XML}${NL}  </testsuite>"
+
+    JUNIT_SUITE_NAME='UNDEFINED_SUITE'
+    JUNIT_SUITE_XML=''  # XML for testcases
+    JUNIT_SUITE_TESTS=0
+    JUNIT_SUITE_PASS=0
+    JUNIT_SUITE_FAIL=0
+    JUNIT_SUITE_DISABLED=0
+}
+
+disabled_test() {
+    JUNIT_SUITE_XML="${JUNIT_SUITE_XML}${NL}    <testcase name=\"${JUNIT_WRAP_TEST}\" status=\"notrun\" time=\"0\" classname=\"${JUNIT_SUITE_NAME}\" />"
+    ((JUNIT_DISABLED += 1))
+    ((JUNIT_SUITE_DISABLED += 1))
+}
+
+
+# -----------------------------------------------------------------------------
+# Make sure necessary environment variables are set up
+# -----------------------------------------------------------------------------
+
+if [ -z "${JUNIT_WRAP_FILE}" ] ; then
+    ( >&2 echo '***** Error: The required environment variable JUNIT_WRAP_FILE is not set' )
+    export JUNIT_WRAP_FILE='junit_wrap_results.xml'
+fi
+
+if [ -z "${JUNIT_WRAP_SUITE}" ] ; then
+    ( >&2 echo '***** Error: The required environment variable JUNIT_WRAP_SUITE is not set' )
+    export JUNIT_WRAP_SUITE='UNDEFINED_TEST'
+fi
+
+if [ -z "${JUNIT_WRAP_TEST}" ] ; then
+    ( >&2 echo '***** Error: The required environment variable JUNIT_WRAP_TEST is not set' )
+    export JUNIT_WRAP_TEST='UNDEFINED_SUITE'
+fi
+
+
+# -----------------------------------------------------------------------------
+# Run the command under test
+# -----------------------------------------------------------------------------
+
+xml_start_suite "${JUNIT_WRAP_SUITE}"
+echo "junit-wrap: Command is [$@]"
+
+eval $@
+declare -i STATUS="$?"
+
+
+# -----------------------------------------------------------------------------
+# Write the XML for this command as a single JUnit test
+# -----------------------------------------------------------------------------
+
+echo "junit-wrap: Command returned ${STATUS}"
+
+if [ "${STATUS}" = 0 ]
+then
+    ((JUNIT_TESTS += 1))
+    ((JUNIT_PASS += 1))
+    ((JUNIT_SUITE_TESTS += 1))
+    ((JUNIT_SUITE_PASS += 1))
+    JUNIT_SUITE_XML="${JUNIT_SUITE_XML}${NL}    <testcase name=\"${JUNIT_WRAP_TEST}\" status=\"run\" time=\"0\" classname=\"${JUNIT_SUITE_NAME}\" />"
+    echo
+    echo "SUCCESS"
+    echo
+    ((NUM_PASSED += 1))
+else
+    local TEST_EXIT_CODE=${?}
+    ((JUNIT_TESTS += 1))
+    ((JUNIT_FAIL += 1))
+    ((JUNIT_SUITE_TESTS += 1))
+    ((JUNIT_SUITE_FAIL += 1))
+    JUNIT_SUITE_XML="${JUNIT_SUITE_XML}${NL}    <testcase name=\"${JUNIT_WRAP_TEST}\" status=\"run\" time=\"0\" classname=\"${JUNIT_SUITE_NAME}\">${NL}      <failure message=\"Test ${JUNIT_WRAP_TEST} failed\" type=\"\" />${NL}    </testcase>"
+    echo
+    echo "FAIL (exit code ${STATUS})"
+    echo
+    ((NUM_FAILED += 1))
+fi
+
+xml_finish_suite
+
+# -----------------------------------------------------------------------------
+# Write the JUnit results to an XML file
+# -----------------------------------------------------------------------------
+
+# Write the XUnit XML file
+echo '<?xml version="1.0" encoding="UTF-8"?>' > "${JUNIT_WRAP_FILE}"
+echo "<testsuites tests=\"${JUNIT_TESTS}\" failures=\"${JUNIT_FAIL}\" disabled=\"${JUNIT_DISABLED}\" errors=\"0\" time=\"0\">" >> "${JUNIT_WRAP_FILE}"
+echo "${JUNIT_XML}" >> "${JUNIT_WRAP_FILE}"
+echo '</testsuites>' >> "${JUNIT_WRAP_FILE}"
+echo "JUnit XML output written to ${JUNIT_WRAP_FILE}"
--- test/ci/run-premerge-ci-checks.sh
+++ test/ci/run-premerge-ci-checks.sh
@@ -13,6 +13,9 @@ echo "**************************************************************************
 
 declare THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Relative to ngraph-tf/build-dir/test, where this script is run
+declare BUILD_DIR="$( realpath .. )"  
+
 if [ -z ${TF_ROOT+x} ]; then
     TF_ROOT="$THIS_SCRIPT_DIR"/../../../tensorflow
 fi
@@ -21,6 +24,9 @@ if [ ! -e $TF_ROOT ]; then
     echo "TensorFlow installation directory not found: " $TF_ROOT
     exit 1
 fi
+
+# Wrapper to produce JUnit XML result output
+declare JUNIT="${THIS_SCRIPT_DIR}/junit-wrap.sh"
 
 # Define default dataset and trained model directories, if not already set
 if [ -z ${NGRAPH_IMAGENET_DATASET+x} ]; then
@@ -37,6 +43,8 @@ fi
 echo "--------------------------------------------------------------------------"
 echo "Running TensorFlow unit tests"
 echo "--------------------------------------------------------------------------"
+
+export GTEST_OUTPUT="xml:${BUILD_DIR}/xunit_gtest.xml"
 ./gtest_ngtf 
 
 ####### Disabled tests for now #######
@@ -51,14 +59,20 @@ echo "--------------------------------------------------------------------------
 echo "--------------------------------------------------------------------------"
 echo "Running test for installation of the ngraph module"
 echo "--------------------------------------------------------------------------"
-python ../../test/install_test.py
+export JUNIT_WRAP_FILE="${BUILD_DIR}/junit_install_test.xml"
+export JUNIT_WRAP_SUITE='installer'
+export JUNIT_WRAP_TEST='install_test.py'
+${JUNIT} python ../../test/install_test.py
 
 echo "--------------------------------------------------------------------------"
 echo "Running a quick inference test"
 echo "--------------------------------------------------------------------------"
 
 pushd ../../examples/resnet
-python tf_cnn_benchmarks.py --model=resnet50 --eval --num_inter_threads=1 \
+export JUNIT_WRAP_FILE="${BUILD_DIR}/junit_resnet50_imagenet_inference.xml"
+export JUNIT_WRAP_SUITE='inference_validation'
+export JUNIT_WRAP_TEST='tf_cnn_benchmarks_resnet50'
+${JUNIT} python tf_cnn_benchmarks.py --model=resnet50 --eval --num_inter_threads=1 \
   --batch_size=16 --num_batches=50 \
   --train_dir "${NGRAPH_TRAINED_MODEL}" \
   --data_format NCHW --select_device NGRAPH \
