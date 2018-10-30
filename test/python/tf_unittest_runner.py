8977ae25378564cb4e12cca76896a85e1eb2e296
sindhu-nervana <43015792+sindhu-nervana@users.noreply.github.com>
2018-10-30 00:06:07 -0700

    Sindhu/tf unittest regexsupport (#282)
    * Add Regex Support for specifying test cases

--- test/python/tf_unittest_runner.py
+++ test/python/tf_unittest_runner.py
@@ -17,6 +17,8 @@ import unittest
 import sys
 import argparse
 import os
+import re
+import fnmatch
 """
 tf_unittest_runner is primarily used to run tensorflow python 
 unit tests using ngraph
@@ -27,6 +29,7 @@ def main():
     parser = argparse.ArgumentParser()
     optional = parser._action_groups.pop()
     required = parser.add_argument_group('required arguments')
+
     required.add_argument(
         '--tensorflow_path',
         help=
@@ -39,89 +42,147 @@ def main():
     optional.add_argument(
         '--run_test',
         help=
-        "Runs the testcase and returns the output. Eg:math_ops_test.math_ops_test.DivNoNanTest.testBasic"
+        "Runs the testcase and returns the output. Eg:math_ops_test.DivNoNanTest.testBasic"
     )
     parser._action_groups.append(optional)
     arguments = parser.parse_args()
 
-    all_dirs_to_path(arguments.tensorflow_path)
-
     if (arguments.list_tests):
-        list_tests(arguments.list_tests)
-
+        test_list = get_test_list(arguments.tensorflow_path,
+                                  arguments.list_tests)
+        print('\n'.join(test_list))
     if (arguments.run_test):
-        run_test(arguments.run_test)
+        test_list = get_test_list(arguments.tensorflow_path, arguments.run_test)
+        run_test(test_list)
+
+
+def get_test_list(tf_path, test_regex):
+    accepted_formats = [
+        "math_ops_test", "mat_ops_test.DivNoNanTest",
+        "math_ops_test.DivNoNanTest.testBasic", "math_ops_test.DivNoNanTest.*",
+        "math_ops_test.D*", "math_ops_test.*", "math_*_test", "math_*_*_test",
+        "math*_test"
+    ]
+    try:
+        module_list = regex_walk(tf_path, test_regex)
+    except:
+        module_list = []
+        print(
+            """Invalid module name. Use bazel query below to get list of tensorflow python test modules.
+            bazel query 'kind(".*_test rule", //tensorflow/python:nn_test)' --output label"""
+        )
+    try:
+        test_list = list_tests(module_list, test_regex)
+    except:
+        test_list = []
+        print(
+            "Enter a valid argument to --list_tests or --run_test.\nList of accepted formats:"
+        )
+        print('\n'.join(accepted_formats))
+    return test_list
 
 
 from fnmatch import fnmatch
 
 
-def all_dirs_to_path(dirname):
+def regex_walk(dirname, regex_input):
     """
     Adds all the directories under the specified dirname to the system path to 
     be able to import the modules.
     
     Args:
-    dir_name: This is the tensorflow_path passed as an argument where 
+    dirname: This is the tensorflow_path passed as an argument where 
     tensorflow is installed.
+    
+    regex_input: Regular expression input string to filter and list/run tests.
+    Few examples of accepted regex_input are:
+    math_ops_test
+    math_ops_test.DivNanTest
+    math_ops_test.DivNoNanTest.testBasic
+    math_ops_test.DivNoNanTest.*
+    math_ops_test.D*
+    math_ops_test.*
+    math_*_test
+    math_*_*_test
+    math*_test
     """
-    pattern = "*_test.py"
+    if (re.search("\.", regex_input) is None):
+        test = regex_input + '.py'
+    else:
+        test = (re.split("\.", regex_input))[0] + '.py'
+    module_list = []
     for path, subdirs, files in os.walk(dirname):
         for name in files:
-            if fnmatch(name, pattern):
+            if fnmatch(name, test):
                 sys.path.append(path)
+                name = os.path.splitext(name)[0]
+                module_list.append(name)
+    if not module_list:
+        sys.exit()
+    return module_list
 
 
-def list_tests(test_module):
+def list_tests(module_list, regex_input):
     """
     Generates a list of test suites and test cases from a TF test target 
     specified. 
 
     Args:
-    test_module: This is tensorflow test target name passed as an argument.
-    Example --list_tests=math_ops_test
-    To get the list of tensorflow python test modules/packages or 
-    labels, query using bazel.
-    bazel query 'kind(".*_test rule", //tensorflow/python/...)'
-    bazel query 'kind(".*_test rule", //tensorflow/python/...)' --output package
+    module_list: This is a list tensorflow test target names passed as an argument.
+    Example --list_tests=math_ops_test.R*
+    To get the list of tensorflow python test modules, query using bazel.
     bazel query 'kind(".*_test rule", //tensorflow/python/...)' --output label
+
+    regex_input: Regular expression input strings to filter and list tests. 
+    Few examples of accepted regex_input are:
+    math_ops_test
+    math_ops_test.DivNanTest
+    math_ops_test.DivNoNanTest.testBasic
+    math_ops_test.DivNoNanTest.*
+    math_ops_test.D*
+    math_ops_test.*
+    math_*_test
+    math_*_*_test
+    math*_test
     """
     loader = unittest.TestLoader()
-    module = __import__(test_module)
-    test_modules = loader.loadTestsFromModule(module)
     alltests = []
-    for test_class in test_modules:
-        alltests.append(([i.id() for i in test_class._tests]))
-    print('\n'.join((sorted(sum(alltests, [])))))
+    for test_module in module_list:
+        module = __import__(test_module)
+        if (module is None):
+            print("Enter a valid test name to run")
+        test_modules = loader.loadTestsFromModule(module)
+        for test_class in test_modules:
+            for i in test_class:
+                alltests.append(i.id())
+
+    if (re.search("\.", regex_input) is None):
+        return alltests
+    else:
+        test_name = (re.split("\*", regex_input))[0]
+        listtests = []
+        for test in alltests:
+            if test_name in test:
+                listtests.append(test)
+        return listtests
 
 
-def run_test(test_name, verbosity=2):
+def run_test(test_list, verbosity=2):
     """
     Runs a specific test suite or test case given with the fully qualified 
     test name and prints stdout.
 
     Args:
-    test_name: This is the test suite or the test case passed as an argument.
-    Example: --run_test=math_ops_test.AccumulateNTest        
+    test_list: This is the list of tests to run,filtered based on the 
+    regex_input passed as an argument.
+    Example: --run_test=math_ops_test.A*   
     verbosity: Python verbose logging is set to 2. You get the help string 
     of every test and the result.
     """
     loader = unittest.TestLoader()
-    tests = loader.loadTestsFromName(test_name)
-    test_result = unittest.TextTestRunner(verbosity=verbosity).run(tests)
-    testsRun = 0
-    tests_run = []
-    failures = []
-    errors = []
-    if test_result.wasSuccessful():
-        tests_run.append([testsRun, 1])
-        sys.exit()
-    elif test_result.errors:
-        errors.append([test_name, test_result.errors])
-        tests_run.append([testsRun, 0])
-    elif test_result.failures:
-        failures.append([test_name, test_result.failures])
-        tests_run.append([testsRun, -1])
+    for test in test_list:
+        tests = loader.loadTestsFromName(test)
+        test_result = unittest.TextTestRunner(verbosity=verbosity).run(tests)
 
 
 if __name__ == '__main__':
