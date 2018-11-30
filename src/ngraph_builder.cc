1683f6d408a231e7be9c7366a1a722d1924b8e6a
Avijit <30507445+avijit-nervana@users.noreply.github.com>
2018-11-30 13:19:03 -0800

    Fixed klockworks errors (#342)

--- src/ngraph_builder.cc
+++ src/ngraph_builder.cc
@@ -3515,20 +3515,26 @@ static Status TranslateStridedSliceOp(
     // if idx is in [-(d-1), d-1], then its same for both inclusive and
     // exclusive
     // The first 2 cases breaks down this range
-    if (idx >= 0 && idx <= (dim - 1)) {
+    if (idx >= 0 && idx <= (static_cast<int>(dim) - 1)) {
       return idx;
-    } else if (idx < 0 && idx + dim >= 0) {  // careful not to do idx >= -dim
-                                             // (since dim is unsigned)
-      return idx + (int)dim;  // Type casting to int to enable unambiguous auto
+    } else if (idx < 0 &&
+               idx + static_cast<int>(dim) >=
+                   0) {  // careful not to do idx >= -dim
+                         // (since dim is unsigned)
+      return idx + static_cast<int>(
+                       dim);  // Type casting to int to enable unambiguous auto
                               // type inference of return type
-    } else if (idx > dim - 1) {
-      return (int)dim;
-    }  // The next case handles the clamping (differently for inclusive and
-       // exclusive cases)
-    else if (idx + dim <
-             0) {  // careful not to do idx < -dim (since dim is unsigned)
+    } else if (idx > static_cast<int>(dim) - 1) {
+      return static_cast<int>(dim);
+    } else if (idx + static_cast<int>(dim) < 0) {
+      // The next case handles the clamping (differently for inclusive and
+      // exclusive cases)
+
+      // careful not to do idx < -dim (since dim is unsigned)
       return 0 - (inclusive ? 0 : 1);
     }
+    // Default case
+    return 0;
   };
 
   auto tf_to_ng = [clamper](int tf_begin_idx, int tf_end_idx, int tf_stride,
