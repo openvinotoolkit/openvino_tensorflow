0694e6551110a7252895c7cd3116d8989a123997
Avijit <30507445+avijit-nervana@users.noreply.github.com>
2019-03-25 23:50:41 -0700

    Updated to nGraph version rc.2 (#470)

--- build_ngtf.py
+++ build_ngtf.py
@@ -86,7 +86,7 @@ def main():
     #-------------------------------
 
     # Component versions
-    ngraph_version = "v0.16.0-rc.0"
+    ngraph_version = "v0.16.0-rc.2"
     tf_version = "v1.13.1"
 
     # Default directories
