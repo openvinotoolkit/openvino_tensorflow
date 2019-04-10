4cdb557d5448ec346a73d41b1385a98f0142d7b7
Christopher Lishka <christopher.r.lishka@intel.com>
2019-04-10 16:02:01 -0700

    Modifications to get python2 builds working again, given how code-format checks have been modified.  Formal code-format check removed from python2 build script because there is now a separate code-format check build task in CI, plus run-premerge-ci-checks.sh runs the code-format check. (#501)

--- test/ci/docker/docker-scripts/run-ngraph-tf-build.sh
+++ test/ci/docker/docker-scripts/run-ngraph-tf-build.sh
@@ -150,14 +150,6 @@ echo ' '
 echo 'Ubuntu version is:'
 cat /etc/os-release
 
-xtime="$(date)"
-echo  ' '
-echo  "===== Starting nGraph TensorFlow Bridge Source Code Format Check at ${xtime} ====="
-echo  ' '
-
-cd "${bridge_dir}"
-maint/check-code-format.sh
-
 # Make sure the Bazel cache is in /tmp, as docker images have too little space
 # in the root filesystem, where /home (and $HOME/.cache) is.  Even though we
 # may not be using the Bazel cache in the builds (in docker), we do this anyway
