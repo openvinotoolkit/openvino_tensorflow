1607499f97ef9af9a0d61b7a696c1404302645aa
avijit-nervana <avijit.chakraborty@intel.com>
2019-04-14 15:28:05 -0700

    Added a check when an update is pushed to a branch (but not a PR)

--- test/ci/buildkite/run_pipeline.sh
+++ test/ci/buildkite/run_pipeline.sh
@@ -4,18 +4,20 @@ set -euo pipefail
 
 echo "BUILDKITE_PULL_REQUEST_REPO: " $BUILDKITE_PULL_REQUEST_REPO
 echo "BUILDKITE_REPO: $BUILDKITE_REPO"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [[ $BUILDKITE_PULL_REQUEST = 'false' ]]; then
+   echo "Not a pull request"
+   cat $SCRIPT_DIR/header.yaml  $SCRIPT_DIR/ngtf-cpu_centos.yaml | buildkite-agent pipeline upload
+else
 if [[ -n \"${BUILDKITE_PULL_REQUEST_REPO##*//}\" && \"${BUILDKITE_REPO##*//}\" != \"${BUILDKITE_PULL_REQUEST_REPO##*//}\" ]]; then
    echo "External Commit"
    export BUILDKITE_CLEAN_CHECKOUT=true
    export BUILDKITE_NO_LOCAL_HOOKS=true
-   cat header.yaml  block_build.yaml ngtf-cpu_centos.yaml | buildkite-agent pipeline upload
+   cat $SCRIPT_DIR/header.yaml  $SCRIPT_DIR/block_build.yaml $SCRIPT_DIR/ngtf-cpu_centos.yaml | buildkite-agent pipeline upload
 else
    echo "Internal Commit"
-   cat header.yaml  ngtf-cpu_centos.yaml | buildkite-agent pipeline upload
+   cat $SCRIPT_DIR/header.yaml  $SCRIPT_DIR/ngtf-cpu_centos.yaml | buildkite-agent pipeline upload
 fi
-
-exit 0
-~                                                                                                                       
-
+fi
 
