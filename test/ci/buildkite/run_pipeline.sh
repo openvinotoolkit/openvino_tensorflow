a938bb677031c2267e922be0a202afd91290fcae
avijit-nervana <avijit.chakraborty@intel.com>
2019-04-14 14:47:26 -0700

    Added a few Buildkite scripts

---
+++ test/ci/buildkite/run_pipeline.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "BUILDKITE_PULL_REQUEST_REPO: " $BUILDKITE_PULL_REQUEST_REPO
+echo "BUILDKITE_REPO: $BUILDKITE_REPO"
+
+if [[ -n \"${BUILDKITE_PULL_REQUEST_REPO##*//}\" && \"${BUILDKITE_REPO##*//}\" != \"${BUILDKITE_PULL_REQUEST_REPO##*//}\" ]]; then
+   echo "External Commit"
+   export BUILDKITE_CLEAN_CHECKOUT=true
+   exort BUILDKITE_NO_LOCAL_HOOKS=true
+   cat header.yaml  block_build.yaml ngtf-cpu_centos.yaml | buildkite-agent pipeline upload
+else
+   echo "Internal Commit"
+   cat header.yaml  ngtf-cpu_centos.yaml | buildkite-agent pipeline upload
+fi
+
+exit 0
+~                                                                                                                       
+
+
