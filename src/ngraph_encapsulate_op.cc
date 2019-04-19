354bc3eb1af8349871044d1e4d61befd4947910c
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-04-19 11:40:07 -0700

    Boolean flip

--- src/ngraph_encapsulate_op.cc
+++ src/ngraph_encapsulate_op.cc
@@ -101,11 +101,10 @@ class NGraphEncapsulateOp : public OpKernel {
       const FunctionDef* fdef =
           flib.Find("Enc_" + to_string(m_ngraph_cluster) + "_native_segment");
       OP_REQUIRES(
-          ctx, fdef == nullptr,
+          ctx, fdef != nullptr,
           errors::Internal("Did not find graphdef for encapsulate ",
                            m_ngraph_cluster,
                            " in NGraphClusterManager or function library"));
-
       // TODO: how to convert from functiondef to graphdef. Anything easier?
       FunctionBody* fnbody;
       const auto get_func_sig = [&flib](const string& op, const OpDef** sig) {
