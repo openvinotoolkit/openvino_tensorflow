2a8c066912ffff0c5afdc1c27c94ba0f288748d7
kanvi-nervana <kanvi.khanna@intel.com>
2019-05-10 16:39:52 -0700

    update license headers (#70)

--- src/tf_deadness_analysis.cc
+++ src/tf_deadness_analysis.cc
@@ -21,7 +21,7 @@ File: tensorflow/tensorflow/compiler/jit/deadness_analysis.cc
 
 *******************************************************************************/
 /*******************************************************************************
- * Copyright 2017-2018 Intel Corporation
+ * Copyright 2017-2019 Intel Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
