065a07d90f86bb22a29ca8e01289f4e0024c146f
Christopher Lishka <christopher.r.lishka@intel.com>
2019-06-06 19:06:12 -0700

    Update the tensorflow version for building ngraph-bridge master, since ngraph-bridge now requires TF 1.14.0rc0 (#101)

--- test/ci/macos/run-ngraph-tf-mac-build.sh
+++ test/ci/macos/run-ngraph-tf-mac-build.sh
@@ -223,7 +223,7 @@ if [ -z "${tf_dir}" ] ; then
     # If installing into the OS, use:
     # sudo --preserve-env --set-home pip install --ignore-installed ${PIP_INSTALL_EXTRA_ARGS:-} "${WHEEL_FILE}"
     # Here we are installing into a virtual environment, so DO NOT USE SUDO!!!
-    pip install -U tensorflow==1.13.1
+    pip install -U tensorflow==1.14.0rc0
     set +x
 
 else 
