cfe133e09fcc33f4d37605dea89e0c4cc3776aee
Sindhura Kantamani <sindhura.kantamani@intel.com>
2019-12-10 17:59:53 -0800

    ngraph v0.27.1-rc.1 (#376)
    upgraded to ngraph v0.27.1-rc.1

--- build_ngtf.py
+++ build_ngtf.py
@@ -53,7 +53,7 @@ def main():
     '''
 
     # Component versions
-    ngraph_version = "v0.27.1-rc.0"
+    ngraph_version = "v0.27.1-rc.1"
     tf_version = "v1.14.0"
 
     # Command line parser options
