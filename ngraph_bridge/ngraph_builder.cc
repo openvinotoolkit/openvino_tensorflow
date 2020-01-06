80803797f69fe64489b650607f0c41ae51dd04ed
maxbubula <maksymilian.bubula@intel.com>
2020-01-06 19:41:54 +0100

    mbubula/BatchMatMul fix (#387)

--- ngraph_bridge/ngraph_builder.cc
+++ ngraph_bridge/ngraph_builder.cc
@@ -893,10 +893,10 @@ static Status TranslateBatchMatMulV2Op(
           "for each dimension. Failed to match dimension ",
           i, " where lhs was ", ng_lhs_shape[i], " and rhs was ",
           ng_rhs_shape[i]);
-    } else if ((ng_lhs_shape[i] == 1) || (ng_rhs_shape[i] == 1)) {
+    } else if (ng_lhs_shape[i] != ng_lhs_shape[i]) {
       return errors::Unimplemented(
           "ng_lhs_shape and ng_rhs_shape must be the same for each dimension, "
-          "for current implementation of BatchMatMulV2."
+          "for current implementation of BatchMatMulV2. "
           "Failed to match dimension ",
           i, " where lhs was ", ng_lhs_shape[i], " and rhs was ",
           ng_rhs_shape[i]);
