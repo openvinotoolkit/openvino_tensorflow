01f4e9e3926dd56ee2a501994fbdf1f3f723c45a
kanvi-nervana <kanvi.khanna@intel.com>
2020-01-15 08:12:33 -0800

    upgrade to ngraphv0.28.0-rc-1 (#437)

--- build_ngtf.py
+++ build_ngtf.py
@@ -53,7 +53,7 @@ def main():
     '''
 
     # Component versions
-    ngraph_version = "v0.28.0-rc.0"
+    ngraph_version = "v0.28.0-rc.1"
     tf_version = "v1.14.0"
 
     # Command line parser options
