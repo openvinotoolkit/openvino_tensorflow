eb5ca013311062353818f1652cf17455d6556531
Abhishek Kulkarni <11399+adk9@users.noreply.github.com>
2020-08-20 14:08:22 -0700

    Update gtest dependency to 1.8.1 (#631)
    * Update to gtest 1.8.1 and use it a dependency
    * Update external_gtest.cmake

--- cmake/external_gtest.cmake
+++ cmake/external_gtest.cmake
@@ -17,72 +17,76 @@
 # Enable ExternalProject CMake module
 include(ExternalProject)
 
-#----------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Download and install GoogleTest ...
-#----------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 SET(GTEST_GIT_REPO_URL https://github.com/google/googletest.git)
-SET(GTEST_GIT_LABEL release-1.8.0)
+SET(GTEST_GIT_LABEL release-1.8.1)
 
-set(CXX_FLAGS "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} -fPIC")
-if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    if (DEFINED NGRAPH_USE_CXX_ABI)
-        set(CXX_FLAGS "${CXX_FLAGS} -D_GLIBCXX_USE_CXX11_ABI=${NGRAPH_USE_CXX_ABI}")
-    endif()
+set(GMOCK_OUTPUT_DIR ${EXTERNAL_PROJECTS_ROOT}/gtest/build/googlemock)
+set(GTEST_OUTPUT_DIR ${GMOCK_OUTPUT_DIR}/gtest)
+
+if(WIN32)
+    list(APPEND GTEST_CMAKE_ARGS
+        -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE=${GTEST_OUTPUT_DIR}
+        -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG=${GTEST_OUTPUT_DIR}
+        -Dgtest_force_shared_crt=TRUE
+    )
+    set(GMOCK_OUTPUT_DIR ${GTEST_OUTPUT_DIR})
 endif()
 
-# The 'BUILD_BYPRODUCTS' argument was introduced in CMake 3.2.
-if (${CMAKE_VERSION} VERSION_LESS 3.2)
-    ExternalProject_Add(
-        ext_gtest
-        GIT_REPOSITORY ${GTEST_GIT_REPO_URL}
-        GIT_TAG ${GTEST_GIT_LABEL}
-        # Disable install step
-        INSTALL_COMMAND ""
-        UPDATE_COMMAND ""
-        CMAKE_ARGS ${CXX_FLAGS}
-            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-        TMP_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/tmp"
-        STAMP_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/stamp"
-        DOWNLOAD_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/download"
-        SOURCE_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/src"
-        BINARY_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/build"
-        INSTALL_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest"
-        )
+if(CMAKE_BUILD_TYPE)
+    list(APPEND GTEST_CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    )
+endif()
+
+if(UNIX)
+    # workaround for compile error
+    # related: https://github.com/intel/mkl-dnn/issues/55
+    set(GTEST_CXX_FLAGS "-Wno-unused-result ${CMAKE_ORIGINAL_CXX_FLAGS} -Wno-undef")
 else()
-    ExternalProject_Add(
-        ext_gtest
-        GIT_REPOSITORY ${GTEST_GIT_REPO_URL}
-        GIT_TAG ${GTEST_GIT_LABEL}
-        # Disable install step
-        INSTALL_COMMAND ""
-        UPDATE_COMMAND ""
-        CMAKE_ARGS ${CXX_FLAGS}
-            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-        TMP_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/tmp"
-        STAMP_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/stamp"
-        DOWNLOAD_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/download"
-        SOURCE_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/src"
-        BINARY_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/build"
-        INSTALL_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest"
-        BUILD_BYPRODUCTS "${EXTERNAL_PROJECTS_ROOT}/gtest/build/googlemock/gtest/libgtest.a"
-        )
+    set(GTEST_CXX_FLAGS ${CMAKE_ORIGINAL_CXX_FLAGS})
 endif()
 
-#----------------------------------------------------------------------------------------------------------
+#Build for ninja
+SET(GTEST_PATHS ${GTEST_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtestd${CMAKE_STATIC_LIBRARY_SUFFIX}
+    ${GMOCK_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gmockd${CMAKE_STATIC_LIBRARY_SUFFIX}
+    ${GTEST_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}
+    ${GMOCK_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX})
 
-get_filename_component(
-    GTEST_INCLUDE_DIR
-    "${EXTERNAL_PROJECTS_ROOT}/gtest/src/googletest/include"
-    ABSOLUTE)
-set(GTEST_INCLUDE_DIR "${GTEST_INCLUDE_DIR}" PARENT_SCOPE)
+ExternalProject_Add(
+    ext_gtest
+    PREFIX gtest
+    GIT_REPOSITORY ${GTEST_GIT_REPO_URL}
+    GIT_TAG ${GTEST_GIT_LABEL}
+    # Disable install step
+    INSTALL_COMMAND ""
+    UPDATE_COMMAND ""
+    CMAKE_GENERATOR ${CMAKE_GENERATOR}
+    CMAKE_GENERATOR_PLATFORM ${CMAKE_GENERATOR_PLATFORM}
+    CMAKE_GENERATOR_TOOLSET ${CMAKE_GENERATOR_TOOLSET}
+    CMAKE_ARGS
+        ${NGRAPH_FORWARD_CMAKE_ARGS}
+        -DCMAKE_CXX_FLAGS=${GTEST_CXX_FLAGS}
+        ${GTEST_CMAKE_ARGS}
+    BINARY_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/build"
+    EXCLUDE_FROM_ALL TRUE
+    BUILD_BYPRODUCTS ${GTEST_PATHS}
+    )
 
-# Create a libgtest target to be used as a dependency by test programs
-add_library(libgtest IMPORTED STATIC GLOBAL)
-add_dependencies(libgtest ext_gtest)
+#------------------------------------------------------------------------------
 
-# Set libgtest properties
-set_target_properties(libgtest PROPERTIES
-    "IMPORTED_LOCATION" "${EXTERNAL_PROJECTS_ROOT}/gtest/build/googlemock/gtest/libgtest.a"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-)
+ExternalProject_Get_Property(ext_gtest SOURCE_DIR BINARY_DIR)
+
+add_library(libgtest INTERFACE)
+add_dependencies(libgtest ext_gtest ext_gmock)
+target_include_directories(libgtest SYSTEM INTERFACE
+    ${SOURCE_DIR}/googletest/include
+    ${SOURCE_DIR}/googlemock/include)
+target_link_libraries(libgtest INTERFACE
+    debug ${GTEST_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtestd${CMAKE_STATIC_LIBRARY_SUFFIX}
+    debug ${GMOCK_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gmockd${CMAKE_STATIC_LIBRARY_SUFFIX}
+    optimized ${GTEST_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}
+    optimized ${GMOCK_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX})
