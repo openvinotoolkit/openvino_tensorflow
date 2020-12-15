892c4bfebc9e68147e19a93cdf6877546b5c6b44
Banikumar Maiti (Intel AIPG) <banikumar.maiti@intel.com>
2020-12-15 10:24:14 -0800

    Add manifest/workdir options to CI models script (#753)
    * set device for run_infer script
    * allow options in run_infer_multiple
    * update models_myriad and models_gpu manifest
    * fix backend in CI yml

--- test/models/tf_ov_model_zoo/run_infer_multiple.sh
+++ test/models/tf_ov_model_zoo/run_infer_multiple.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 # This script is used by BuildKite CI to fetch/run multiple models from a curated model-repo for OV-IE integration project
-# Invoke locally: .../run_infer_multiple.sh [ ./models_cpu.txt ]  [ .../working_dir ]
+# Invoke locally: .../run_infer_multiple.sh [ -m ./models_cpu.txt ]  [ -d .../working_dir ]
+
+usage() { echo "Usage: $0 [-m .../manifest.txt] [-d .../working_dir]" 1>&2; exit 1; }
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+WORKDIR=`pwd`
 device=${NGRAPH_TF_BACKEND:-"CPU"}
 device="${device,,}" # lowercase
 MODELFILENAME=models_${device}.txt
 # read models & params from manifest
 MANIFEST=${SCRIPT_DIR}/${MODELFILENAME}
-[ -z $1 ] || MANIFEST=$1
+
+while getopts “:m:d:h” opt; do
+  case $opt in
+    m) MANIFEST=${OPTARG} ;;
+    d) WORKDIR=${OPTARG} ;;
+    h) usage ;;
+    *) usage ;;
+  esac
+done
+shift $((OPTIND-1))
+
 [ -f "$MANIFEST" ] || ( echo "Manifest not found: $MANIFEST !"; exit 1 )
 MANIFEST="$(cd "$(dirname "$MANIFEST")"; pwd)/$(basename "$MANIFEST")" # absolute path
 
-WORKDIR=`pwd`
-[ -z $2 ] || WORKDIR=$2
 cd ${WORKDIR} || ( echo "Not found: $WORKDIR !"; exit 1 )
 echo "Dir: ${WORKDIR}"
 
