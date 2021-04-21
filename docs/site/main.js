82b6962c9a38d21c3baaa6379b164ef39d31e23f
barykrg <barykrg@gmail.com>
2021-04-21 13:11:50 +0530

    Sentence formation
    Changed Sentence formation for website.

--- docs/site/main.js
+++ docs/site/main.js
@@ -125,7 +125,7 @@ function commandArea()
     if(!command)
     {
       let invalidConfig = document.createElement('b');
-      invalidConfig.innerHTML = "Please click the required configuration in the above interactive table to display the right command";
+      invalidConfig.innerHTML = "Please click the required configuration in the interactive table to display the right command for installation";
       area.innerHTML = "";
       area.appendChild(invalidConfig);
     }
