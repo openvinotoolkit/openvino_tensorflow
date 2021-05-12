c1aba2e02a90e7457812d15ec80c7f310732f497
Barun <barykrg@gmail.com>
2021-05-12 20:20:16 +0530

    Changed folder structure

--- tools/builds/Docker/build_docker.py
+++ tools/builds/Docker/build_docker.py
@@ -3,25 +3,29 @@ import subprocess
 from bs4 import BeautifulSoup
 
 
+#Creates Docker Files for each base image and for each combination
+base_docker_files=os.listdir("./docker_files")
+print(base_docker_files)
 success_build={}
-Soup = BeautifulSoup(open('build_options.xml','r'),'lxml')
-for bo in Soup.find_all(['item']):
-    if os.path.exists(bo.find(['name']).text):
-        os.remove(bo.find(['name']).text)
-     # Use the name of approprite docker file
-     #eg for ubuntu20 use Dockerfile.ubuntu20.04_base    
-    base = open("Dockerfile.ubuntu20.04_base", "r")
-    temp = open(bo.find(['name']).text,"w")
-    for line in base:
-        temp.write(line)
-    for line in bo.find_all(['command']):
-        temp.write("RUN "+line.text+"\n")
-    temp.close()
-    base.close()
+for bdf in base_docker_files:
+    Soup = BeautifulSoup(open('build_options.xml','r'),'lxml')
+    for bo in Soup.find_all(['item']):
+        if os.path.exists(bdf+"_"+bo.find(['name']).text):
+            os.remove(bdf+"_"+bo.find(['name']).text)
+        # Use the name of approprite docker file
+        #eg for ubuntu20 use Dockerfile.ubuntu20.04_base    
+        base = open("./docker_files/"+bdf, "r")
+        temp = open(bdf+"_"+bo.find(['name']).text,"w")
+        for line in base:
+            temp.write(line)
+        for line in bo.find_all(['command']):
+            temp.write("RUN "+line.text+"\n")
+        temp.close()
+        base.close()
 
-# # If proxy settings are detected in the environment, make sure they are
-# # included on the docker-build command-line.  This mirrors a similar system
-# # in the Makefile.
+# # # If proxy settings are detected in the environment, make sure they are
+# # # included on the docker-build command-line.  This mirrors a similar system
+# # # in the Makefile.
 DOCKER_HTTP_PROXY=''
 DOCKER_HTTPS_PROXY=''
 DOCKER_FTP_PROXY=''
@@ -39,21 +43,22 @@ if os.environ.__contains__('ftp_proxy'):
 if os.environ.__contains__('no_proxy'):
     DOCKER_NO_PROXY="--build-arg no_proxy="+os.environ['no_proxy']
 
-
-for bo in Soup.find_all(['item']):
-    command = ("docker build --pull --rm %s %s %s %s -t %s:latest -f %s  ." %(DOCKER_HTTP_PROXY,DOCKER_HTTPS_PROXY,DOCKER_NO_PROXY,DOCKER_FTP_PROXY,bo.find(['name']).text,bo.find(['name']).text))
-    print(command)
-    output=subprocess.run(command,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if os.path.exists(bo.find(['name']).text+".log"):
-        os.remove(bo.find(['name']).text+".log")
-    logging = open(bo.find(['name']).text+".log","w")
-    logging.write("**************STDOUT**********************\n")
-    logging.write(output.stdout.decode('UTF-8'))
-    logging.write("**************STDERR**********************\n")
-    logging.write(output.stderr.decode('UTF-8'))
-    logging.close()
-    success = "Success" if output.returncode==0 else "Failure"
-    success_build[bo.find(['name']).text]=success
+#Run docker files
+for bdf in base_docker_files:
+    for bo in Soup.find_all(['item']):
+        command = ("docker build --rm %s %s %s %s -t %s:latest -f %s  ." %(DOCKER_HTTP_PROXY,DOCKER_HTTPS_PROXY,DOCKER_NO_PROXY,DOCKER_FTP_PROXY,bdf+"_"+bo.find(['name']).text,bdf+"_"+bo.find(['name']).text))
+        print(command)
+        output=subprocess.run(command,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if os.path.exists(bdf+"_"+bo.find(['name']).text+".log"):
+            os.remove(bdf+"_"+bo.find(['name']).text+".log")
+        logging = open(bdf+"_"+bo.find(['name']).text+".log","w")
+        logging.write("**************STDOUT**********************\n")
+        logging.write(output.stdout.decode('UTF-8'))
+        logging.write("**************STDERR**********************\n")
+        logging.write(output.stderr.decode('UTF-8'))
+        logging.close()
+        success = "Success" if output.returncode==0 else "Failure"
+        success_build[bdf+"_"+bo.find(['name']).text]=success
 
 if os.path.exists("overallstatus.txt"):
     os.remove("overallstatus.txt")
