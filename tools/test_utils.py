d35b2280fcda675e71c62ce2325837feed4afd9b
barykrg <barykrg@gmail.com>
2021-06-09 09:25:45 +0530

    format changing for test_util.py

--- tools/test_utils.py
+++ tools/test_utils.py
@@ -28,7 +28,8 @@ class TestEnv:
 
     @staticmethod
     def get_linux_type():
-        linux_distro = subprocess.check_output("""awk -F= '$1=="ID" { print $2 ;}' /etc/os-release""", shell=True)
+        linux_distro = subprocess.check_output(
+            """awk -F= '$1=="ID" { print $2 ;}' /etc/os-release""", shell=True)
         if "ubuntu" in linux_distro.decode("utf-8"):
             return 'Ubuntu'
         elif "centos" in linux_distro.decode("utf-8"):
