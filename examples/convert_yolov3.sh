1f94f3b6a6f76d3d4b30710c7f1703c8ad5a44a8
adisan3 <adityax.sanivarapu@intel.com>
2021-08-30 18:00:04 +0530

    saving the .h5 keras format for yolov3 darknet

--- examples/convert_yolov3.sh
+++ examples/convert_yolov3.sh
@@ -17,6 +17,7 @@ wget -O weights/yolov3.weights https://pjreddie.com/media/files/yolov3.weights
 python tools/model_converter/convert.py cfg/yolov3.cfg weights/yolov3.weights weights/darknet53.h5
 python tools/model_converter/keras_to_tensorflow.py --input_model weights/darknet53.h5 --output_model=weights/${model_name}.pb
 cp weights/${model_name}.pb ../../data/${model_name}.pb
+cp weights/darknet53.h5 ../../data/darknet53.h5
 cp coco.names ../../data/
 cd ../..
 
