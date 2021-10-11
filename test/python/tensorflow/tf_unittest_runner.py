5438c65bc430ff0138c60da32b58cdac833ac712
pratiksha123507 <pratikshax.bapusaheb.vanse@intel.com>
2021-10-11 14:01:26 +0530

    check for execution of untrusted input

--- test/python/tensorflow/tf_unittest_runner.py
+++ test/python/tensorflow/tf_unittest_runner.py
@@ -25,7 +25,7 @@ if not platform.system() == "Darwin":
 try:
     import xmlrunner
 except:
-    subprocess.Popen(shlex.split("pip install unittest-xml-reporting"))
+    subprocess.run(shlex.split("pip install unittest-xml-reporting"), stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     
     import xmlrunner
 """
