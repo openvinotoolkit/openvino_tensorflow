375bed84af30047c41a2e4ab41e1a8fdc12a5703
Laxmi Ganesan <laxmi.ganesan@intel.com>
2021-11-02 17:06:09 -0700

    Updating classification sample to TF2.x

--- examples/classification_sample.py
+++ examples/classification_sample.py
@@ -31,11 +31,12 @@ import os
 import numpy as np
 import tensorflow as tf
 import openvino_tensorflow as ovtf
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.models import load_model
 import time
 import cv2
 
-from common.utils import get_input_mode, load_graph
-
+from common.utils import get_input_mode
 
 def read_tensor_from_image_file(frame,
                                 input_height=299,
@@ -49,7 +50,6 @@ def read_tensor_from_image_file(frame,
     result = np.expand_dims(normalized_image, 0)
     return result
 
-
 def load_labels(label_file):
     label = []
     proto_as_ascii_lines = tf.io.gfile.GFile(label_file).readlines()
@@ -60,14 +60,14 @@ def load_labels(label_file):
 
 if __name__ == "__main__":
     input_file = "examples/data/grace_hopper.jpg"
-    model_file = "examples/data/inception_v3_2016_08_28_frozen.pb"
-    label_file = "examples/data/imagenet_slim_labels.txt"
+    model_file = ""
+    label_file = tf.keras.utils.get_file(
+    'ImageNetLabels.txt',
+    'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
     input_height = 299
     input_width = 299
     input_mean = 0
     input_std = 255
-    input_layer = "input"
-    output_layer = "InceptionV3/Predictions/Reshape_1"
     backend_name = "CPU"
 
     # overlay parameters
@@ -78,10 +78,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--graph", help="Optional. Path to graph/model to be executed.")
-    parser.add_argument("--input_layer", help="Optional. Name of input layer.")
-    parser.add_argument(
-        "--output_layer", help="Optional. Name of output layer.")
+        "--model", help="Optional. Path to model to be executed.")
     parser.add_argument(
         "--labels", help="Optional. Path to labels mapping file.")
     parser.add_argument(
@@ -96,10 +93,6 @@ if __name__ == "__main__":
     parser.add_argument(
         "--input_width", type=int, help="Optional. Specify input width value.")
     parser.add_argument(
-        "--input_mean", type=int, help="Optional. Specify input mean value.")
-    parser.add_argument(
-        "--input_std", type=int, help="Optional. Specify input std value.")
-    parser.add_argument(
         "--backend",
         help="Optional. Specify the target device to infer on; "
         "CPU, GPU, MYRIAD or VAD-M is acceptable. Default value is CPU.")
@@ -111,16 +104,8 @@ if __name__ == "__main__":
         action='store_true')
     args = parser.parse_args()
 
-    if args.graph:
-        model_file = args.graph
-        if not args.input_layer:
-            raise Exception("Specify input layer for this network")
-        else:
-            input_layer = args.input_layer
-        if not args.output_layer:
-            raise Exception("Specify output layer for this network")
-        else:
-            output_layer = args.output_layer
+    if args.model:
+        model_file = args.model
         if args.labels:
             label_file = args.labels
         else:
@@ -131,19 +116,13 @@ if __name__ == "__main__":
         input_height = args.input_height
     if args.input_width:
         input_width = args.input_width
-    if args.input_mean:
-        input_mean = args.input_mean
-    if args.input_std:
-        input_std = args.input_std
     if args.backend:
         backend_name = args.backend
 
-    graph = load_graph(model_file)
-
-    input_name = "import/" + input_layer
-    output_name = "import/" + output_layer
-    input_operation = graph.get_operation_by_name(input_name)
-    output_operation = graph.get_operation_by_name(output_name)
+    if model_file == "":
+        model = InceptionV3(weights='imagenet')
+    else:
+        model = load_model(model_file)
 
     if not args.disable_ovtf:
         #Print list of available backends
@@ -175,69 +154,67 @@ if __name__ == "__main__":
         )
     images_len = len(images)
     # Initialize session and run
-    config = tf.compat.v1.ConfigProto()
     image_id = -1
-    with tf.compat.v1.Session(graph=graph, config=config) as sess:
-        while True:
-            image_id += 1
-            if input_mode in ['camera', 'video']:
-                if cap.isOpened():
-                    ret, frame = cap.read()
-                    if ret is True:
-                        pass
-                    else:
-                        break
+    while True:
+        image_id += 1
+        if input_mode in ['camera', 'video']:
+            if cap.isOpened():
+                ret, frame = cap.read()
+                if ret is True:
+                    pass
                 else:
                     break
-            if input_mode in ['image', 'directory']:
-                if image_id < images_len:
-                    frame = cv2.imread(images[image_id])
-                else:
-                    break
-            t = read_tensor_from_image_file(
+            else:
+                break
+        if input_mode in ['image', 'directory']:
+            if image_id < images_len:
+                frame = cv2.imread(images[image_id])
+            else:
+                break
+
+        t = read_tensor_from_image_file(
                 frame,
                 input_height=input_height,
                 input_width=input_width,
                 input_mean=input_mean,
                 input_std=input_std)
 
-            # Warmup
-            if image_id == 0:
-                results = sess.run(output_operation.outputs[0],
-                                   {input_operation.outputs[0]: t})
-            # run
-            start = time.time()
-            results = sess.run(output_operation.outputs[0],
-                               {input_operation.outputs[0]: t})
-            elapsed = time.time() - start
-            fps = 1 / elapsed
-            print('Inference time in ms: %.2f' % (elapsed * 1000))
-            results = np.squeeze(results)
-            if label_file:
-                cv2.putText(frame,
-                            'Inference Running on : {0}'.format(backend_name),
-                            (30, 50), font, font_size, color, font_thickness)
-                cv2.putText(
-                    frame, 'FPS : {0} | Inference Time : {1}ms'.format(
-                        int(fps), round((elapsed * 1000), 2)), (30, 80), font,
-                    font_size, color, font_thickness)
-                top_k = results.argsort()[-5:][::-1]
-                c = 130
-                for i in top_k:
+        # Warmup
+        if image_id == 0:
+            results = model.predict(t)
+
+        # run
+        start = time.time()
+        results = model.predict(t)
+        elapsed = time.time() - start
+        fps = 1 / elapsed
+        print('Inference time in ms: %.2f' % (elapsed * 1000))
+        results = np.squeeze(results)
+
+        if label_file:
+            cv2.putText(frame,
+                        'Inference Running on : {0}'.format(backend_name),
+                        (30, 50), font, font_size, color, font_thickness)
+            cv2.putText(
+                frame, 'FPS : {0} | Inference Time : {1}ms'.format(
+                    int(fps), round((elapsed * 1000), 2)), (30, 80), font,
+                font_size, color, font_thickness)
+            top_k = results.argsort()[-5:][::-1]
+            c = 130
+            for i in top_k:
                     cv2.putText(frame, '{0} : {1}'.format(
                         labels[i], results[i]), (30, c), font, font_size, color,
                                 font_thickness)
-                    print(labels[i], results[i])
-                    c += 30
-            else:
-                print(
-                    "No label file provided. Cannot print classification results"
-                )
-            if not args.no_show:
-                cv2.imshow("results", frame)
-                if cv2.waitKey(1) & 0XFF == ord('q'):
-                    break
-    sess.close()
+                    print(labels[i+1], results[i])
+                    c+=30
+        else:
+            print(
+                "No label file provided. Cannot print classification results"
+            )
+        if not args.no_show:
+            cv2.imshow("results", frame)
+            if cv2.waitKey(1) & 0XFF == ord('q'):
+                break
     if cap:
         cap.release()
         cv2.destroyAllWindows()
