229c30b81745db9f7eb2bf015bc7576317cf1d59
Preetha Veeramalai <preetha.veeramalai@intel.com>
2021-11-10 21:54:55 +0530

    Remove HDDL device specific op checks

--- openvino_tensorflow/assign_clusters.cc
+++ openvino_tensorflow/assign_clusters.cc
@@ -412,57 +412,6 @@ Status AssignClusters(Graph* graph) {
   std::unordered_map<std::string, tuple<string, string, vector<string>>>
       deadness_info;
 
-  string device;
-  BackendManager::GetBackendName(device);
-
-  // Drop Shape if it's the output node of HDDL cluster.
-  // Drop Sub if it's the input node and the input is from a Const node.
-  if (device.find("HETERO") == std::string::npos){
-    if (device == "HDDL") {
-      for (auto edge : graph->edges()) {
-        Node* src = edge->src();
-        Node* dst = edge->dst();
-        if (!src->IsOp() || !dst->IsOp()) {
-          if (src->type_string() == "Shape") {
-            src->ClearAttr("_ovtf_marked_for_clustering");
-          }
-          continue;
-        }
-        if (!NodeIsMarkedForClustering(src) || !NodeIsMarkedForClustering(dst)) {
-          if (src->type_string() == "Const" && dst->type_string() == "Sub") {
-            dst->ClearAttr("_ovtf_marked_for_clustering");
-          }
-          if (src->type_string() == "Shape") {
-            src->ClearAttr("_ovtf_marked_for_clustering");
-          }
-          continue;
-        }
-        bool is_deadness_ok = false;
-        TF_RETURN_IF_ERROR(
-            CanContractEdgeDeadnessCheck(edge, cluster_map, is_deadness_ok));
-        if (!is_deadness_ok) {
-          if (src->type_string() == "Const" && dst->type_string() == "Sub") {
-            dst->ClearAttr("_ovtf_marked_for_clustering");
-          }
-          if (src->type_string() == "Shape") {
-            src->ClearAttr("_ovtf_marked_for_clustering");
-          }
-          continue;
-        }
-        int src_index = cluster_map[src]->index;
-        int dst_index = cluster_map[dst]->index;
-        if (!(gc.HasEdge(src_index, dst_index) &&
-              gc.CanContractEdge(src_index, dst_index))) {
-          if (src->type_string() == "Const" && dst->type_string() == "Sub") {
-            dst->ClearAttr("_ovtf_marked_for_clustering");
-          }
-          if (src->type_string() == "Shape") {
-            src->ClearAttr("_ovtf_marked_for_clustering");
-          }
-        }
-      }
-    }
-  }
   do {
     changed = false;
 
