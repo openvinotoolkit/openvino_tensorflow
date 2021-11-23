f45e7d8cb287ca5b436e5fc350603c077bacdff6
Suryaprakash Shanmugam <suryaprakash.shanmugam@intel.com>
2021-11-23 16:48:13 +0530

    Upgrade OpenVINO Opset to version 7

--- openvino_tensorflow/backend.cc
+++ openvino_tensorflow/backend.cc
@@ -87,8 +87,8 @@ std::string Backend::GetDeviceType() { return m_device_type; }
 bool Backend::IsSupported(const Node& node) const {
   // TODO: check if the given backend/device supports the op. Right now we're
   // assuming
-  // that the selected backend supports all opset5 ops
-  const auto& opset = ngraph::get_opset5();
+  // that the selected backend supports all opset7 ops
+  const auto& opset = ngraph::get_opset7();
   return opset.contains_op_type(&node);
 }
 
--- openvino_tensorflow/mark_for_clustering.cc
+++ openvino_tensorflow/mark_for_clustering.cc
@@ -135,6 +135,7 @@ const std::map<std::string, SetAttributesFunction>& GetAttributeSetters() {
     set_attributes_map["ConcatV2"] = SetStaticInputs({-1});
     set_attributes_map["Conv2DBackpropInput"] = SetStaticInputs({0});
     set_attributes_map["CropAndResize"] = SetStaticInputs({1, 2, 3});
+    set_attributes_map["Einsum"] = SetStaticInputs({0});
     set_attributes_map["ExpandDims"] = SetStaticInputs({1});
     set_attributes_map["Fill"] = SetStaticInputs({0});
     set_attributes_map["GatherV2"] = SetStaticInputs({2});
