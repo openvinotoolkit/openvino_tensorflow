ccdd9cc36360c5fb5bd9fdf147ab1715b4af44ea
Chandrakant Khandelwal <chandrakant.khandelwal@intel.com>
2022-01-27 17:26:32 +0530

    Comment ParseNodeAttributes call in Ngraphencapsulate, as it is unused and causing error for some models in windows

--- openvino_tensorflow/kernels/encapsulate_op.cc
+++ openvino_tensorflow/kernels/encapsulate_op.cc
@@ -190,10 +190,10 @@ NGraphEncapsulateOp::NGraphEncapsulateOp(OpKernelConstruction* ctx)
   }
 
   // Get the optional attributes
-  std::unordered_map<std::string, std::string> additional_attribute_map;
-  auto node_def = ctx->def();
-  OP_REQUIRES_OK(
-      ctx, ParseNodeAttributes(node_def.attr(), &additional_attribute_map));
+  // std::unordered_map<std::string, std::string> additional_attribute_map;
+  // auto node_def = ctx->def();
+  // OP_REQUIRES_OK(
+  //     ctx, ParseNodeAttributes(node_def.attr(), &additional_attribute_map));
 }
 
 NGraphEncapsulateOp::~NGraphEncapsulateOp() {
