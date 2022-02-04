325de30f70c4ac775265d54bd96c72bd1bec0918
Cavus Mustafa <mustafa.cavus@intel.com>
2022-02-03 18:21:12 -0800

    Additional OV2.0 updates and VADM OV2.0

--- openvino_tensorflow/ie_basic_engine.cc
+++ openvino_tensorflow/ie_basic_engine.cc
@@ -34,12 +34,10 @@ void IE_Basic_Engine::infer(
     m_infer_reqs.push_back(m_compiled_model.create_infer_request());
   }
 
-  std::cout << "OVTF_LOG - IE_Basic_Engine - A" << std::endl;
   //  Prepare input blobs
   auto parameters = m_model->get_parameters();
   for (int i = 0; i < inputs.size(); i++) {
     if (inputs[i] != nullptr) {
-        ov::Tensor tensor(inputs[i]->get_element_type(), inputs[i]->get_shape(), inputs[i]->get_data_ptr());
 #if defined(OPENVINO_2021_2)
       if (m_device != "MYRIAD" && m_device != "HDDL") {
         OVTF_VLOG(4) << "IE_Basic_Engine::infer() set_input_tensor() ("
@@ -93,7 +91,6 @@ void IE_Basic_Engine::infer(
   //  Prepare output blobs
   auto results = m_model->get_results();
   for (int i = 0; i < results.size(); i++) {
-    ov::Tensor tensor(outputs[i]->get_element_type(), outputs[i]->get_shape(), outputs[i]->get_data_ptr());
     if (outputs[i] != nullptr) {
       OVTF_VLOG(4) << "IE_Basic_Engine::infer() set_output_tensor() ("
                    << output_names[i] << ")";
