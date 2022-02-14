0d4694025c7ab55c695816bac84d5d5838996039
chandrakant khandelwal <chandrakant.khandelwal@intel.com>
2022-02-14 13:36:47 +0530

    Update build scripts, as tensroflow_frontend lib is renamed to tensorflow_fe, and now being installed using cmake in OV code

--- build_ovtf.py
+++ build_ovtf.py
@@ -557,27 +557,6 @@ def main():
 
         build_openvino(build_dir, openvino_src_dir, cxx_abi, target_arch,
                        artifacts_location, arguments.debug_build, verbosity)
-        # TODO Copy tensorflow_frontend.so and the header files for now,
-        # remove it once, it is handled in OV cmakelist
-        # cwd is "build_cmake/"
-        command_executor([
-            "cp", "-r",
-            "openvino/src/frontends/tensorflow/include/openvino/frontend/tensorflow",
-            "artifacts/openvino/runtime/include/openvino/frontend/"
-        ])
-
-        if (os.path.isdir("openvino/bin/intel64/Release")):
-            command_executor([
-                "cp",
-                "openvino/bin/intel64/Release/lib/libopenvino_tensorflow_frontend.so",
-                "artifacts/openvino/runtime/lib/intel64/"
-            ])
-        else:
-            command_executor([
-                "cp",
-                "openvino/bin/intel64/lib/libopenvino_tensorflow_frontend.so",
-                "artifacts/openvino/runtime/lib/intel64/"
-            ])
 
     # Next build CMAKE options for the bridge
     atom_flags = ""
