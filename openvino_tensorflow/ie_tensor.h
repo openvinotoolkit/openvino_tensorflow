15ccd7ca0ded8ff7dbab6811bd34681bba14e7c4
Mustafa Cavus <mustafa.cavus@intel.com>
2022-03-22 02:42:44 -0700

    Dyn output copy added, zero-copy removed (#282)

--- openvino_tensorflow/ie_tensor.h
+++ openvino_tensorflow/ie_tensor.h
@@ -30,26 +30,5 @@ class IETensor : public ov::Tensor {
   IETensor& operator=(const IETensor&) = delete;
 };
 
-// A simple TensorBuffer implementation that allows us to create Tensors that
-// take ownership of pre-allocated memory.
-class IETensorBuffer : public TensorBuffer {
- public:
-  IETensorBuffer(std::shared_ptr<IETensor> tensor)
-      : TensorBuffer(const_cast<void*>(tensor->data())),
-        size_(tensor->get_byte_size()),
-        tensor_(tensor) {}
-
-  size_t size() const override { return size_; }
-
-  TensorBuffer* root_buffer() override { return this; }
-
-  void FillAllocationDescription(AllocationDescription* proto) const override {
-    proto->set_allocated_bytes(size_);
-  }
-
- private:
-  size_t size_;
-  std::shared_ptr<IETensor> tensor_;
-};
 }  // namespace openvino_tensorflow
 }  // namespace tensorflow
