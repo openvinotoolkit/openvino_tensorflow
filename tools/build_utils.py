2732c093266a13b065303b1254aba348125fc0dd
Gowthami D <gowthamix.d@intel.com>
2022-03-22 17:32:23 +0530

    Update build_utils.py

--- tools/build_utils.py
+++ tools/build_utils.py
@@ -253,7 +253,7 @@ def setup_venv(venv_dir,tf_version):
     ]
     command_executor(package_list)
     # TF >=2.4 and <= 2.6 requires numpy~=1.19.2
-    if (int(tf_version.split(".")[1]) > 3 & int(tf_version.split(".")[1]) < 7):
+    if ((int(tf_version.split(".")[1]) > 3) & (int(tf_version.split(".")[1]) < 7)):
         command_executor(["pip", "install", "h5py"])
         command_executor(["pip", "install", "numpy~=1.19.2"])
     # TF on windows needs a higher version of numpy
