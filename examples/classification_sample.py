658893a764b920060d17e38e9a85c7a3bca3d87d
Chandrakant Khandelwal <chandrakant.khandelwal@intel.com>
2022-03-28 22:25:50 +0530

    Update OCM submodule and fix some checks in the examples (#287)
    * Added a condition check for images
    
    * Update OCM submodule , new changes are yriad range op fix andd updated UTs
    
    * Add fixes for some path checks

--- examples/classification_sample.py
+++ examples/classification_sample.py
@@ -29,6 +29,10 @@ from __future__ import print_function
 
 import argparse
 import os
+# Enable these variables for runtime inference optimizations
+os.environ["OPENVINO_TF_CONVERT_VARIABLES_TO_CONSTANTS"] = "1"
+os.environ[
+    "TF_ENABLE_ONEDNN_OPTS"] = "1"  # This needs to be set before importing TF
 import numpy as np
 import tensorflow as tf
 import openvino_tensorflow as ovtf
@@ -38,10 +42,6 @@ import time
 import cv2
 
 from common.utils import get_input_mode
-
-# Enable these variables for runtime inference optimizations
-os.environ["OPENVINO_TF_CONVERT_VARIABLES_TO_CONSTANTS"] = "1"
-os.environ["TF_ENABLE_ONEDNN_OPTS"] = "1"
 
 
 def preprocess_image(frame,
@@ -167,7 +167,14 @@ if __name__ == "__main__":
     elif input_mode == 'image':
         images = [input_file]
     elif input_mode == 'directory':
-        images = [os.path.join(input_file, i) for i in os.listdir(input_file)]
+        if not os.path.isdir(input_file):
+            raise AssertionError("Path doesn't exist {0}".format(input_file))
+        images = [
+            os.path.join(input_file, fname)
+            for fname in os.listdir(input_file)
+            if fname.lower().endswith(('.png', '.jpg', '.jpeg', '.tif', '.tiff',
+                                       '.bmp'))
+        ]
     else:
         raise Exception(
             "Invalid input. Path to an image or video or directory of images. Use 0 for using camera as input."
