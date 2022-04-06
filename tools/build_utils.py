29b4331c39ce05efb3253e2ae45024dfb8dfed86
adisan3 <adityax.sanivarapu@intel.com>
2022-04-06 17:55:27 +0530

    installing opecv during venv setup

--- tools/build_utils.py
+++ tools/build_utils.py
@@ -179,6 +179,7 @@ def setup_venv(venv_dir):
         "keras_preprocessing>=1.1.1,<1.2",
         "--no-deps",
         "yapf==0.26.0",
+        "opencv-python==4.5.2.54",
     ]
     command_executor(package_list)
 
