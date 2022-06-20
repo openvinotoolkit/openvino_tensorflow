f0e643ac4f9710925d8134e4d741f52ebd15962e
Srihari Humbarwadi <srihari.humbarwadi@intel.com>
2022-06-20 17:25:46 +0530

    Fixes CI build failures (#314)
    * fixed protobuf version bug
    
    * cleanup: removed references to `infer_duration_in_ms`
    
    * Revert optimize_graph call in classification sample
    
    * cleanup post-processing
    
    Co-authored-by: Suryaprakash Shanmugam <suryaprakash.shanmugam@intel.com>

--- openvino_tensorflow/ie_basic_engine.cc
+++ openvino_tensorflow/ie_basic_engine.cc
@@ -29,7 +29,7 @@ void IE_Basic_Engine::infer(
     std::vector<std::shared_ptr<IETensor>>& outputs,
     std::vector<std::string>& output_names,
     std::vector<std::shared_ptr<IETensor>>& hoisted_params,
-    std::vector<std::string>& param_names, int64_t* infer_duration_in_ms) {
+    std::vector<std::string>& param_names) {
   load_network();
   if (m_infer_reqs.empty()) {
     m_infer_reqs.push_back(m_compiled_model.create_infer_request());
@@ -100,9 +100,7 @@ void IE_Basic_Engine::infer(
     }
   }
 
-  int64_t start_ns = profiler::GetCurrentTimeNanos();
   m_infer_reqs[0].infer();
-  *infer_duration_in_ms = (profiler::GetCurrentTimeNanos() - start_ns) / 1e6;
 
   // Set dynamic output blobs
   for (int i = 0; i < results.size(); i++) {
