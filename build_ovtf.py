c7f9857ab7748edae2ca4f427e048c441abc05ed
Suryaprakash Shanmugam <suryaprakash.shanmugam@intel.com>
2022-08-18 08:47:15 +0530

    Changed default OV to TF-FE dev. branch
    (cherry picked from commit a89ba3c28185067dfccd6443f076ea47e3393153)

--- build_ovtf.py
+++ build_ovtf.py
@@ -117,7 +117,7 @@ def main():
     parser.add_argument(
         '--openvino_version',
         help="Openvino version to be used for building from source",
-        default='2022.1.0')
+        default='rkazants/tf_fe_2022_1_integration')
 
     parser.add_argument(
         '--python_executable',
@@ -174,7 +174,7 @@ def main():
     # Default directories
     build_dir = arguments.build_dir
 
-    if (arguments.openvino_version not in ["master", "2022.1.0"]):
+    if (arguments.openvino_version not in ["master", "2022.1.0", "rkazants/tf_fe_2022_1_integration"]):
         raise AssertionError(
             "Only 2022.1.0 OpenVINO version and master branch are supported")
 
@@ -438,15 +438,30 @@ def main():
             openvino_release_tag = "master"
         elif (arguments.openvino_version == "2022.1.0"):
             openvino_release_tag = "2022.1.0"
+        elif (arguments.openvino_version == "rkazants/tf_fe_2022_1_integration"):
+            openvino_release_tag = "rkazants/tf_fe_2022_1_integration"
 
         # Download OpenVINO
         download_repo(
             "openvino",
-            "https://github.com/openvinotoolkit/openvino.git",
+            #TODO: Switch back to the main OV repo before merging
+            #"https://github.com/openvinotoolkit/openvino.git",
+            "https://github.com/rkazants/openvino.git",
             openvino_release_tag,
             submodule_update=True)
         openvino_src_dir = os.path.join(os.getcwd(), "openvino")
         print("OV_SRC_DIR: ", openvino_src_dir)
+
+        # TODO: Temporary workaround for the protobuf issue.
+        #       Remove after the issue is fixed.
+        pwd = os.getcwd()
+        os.chdir(openvino_src_dir)
+        command_executor(["git", "apply", "../../ov_tf_fe.patch"])
+        command_executor(["git", "submodule", "update", "--init", "--recursive"])
+        os.chdir(openvino_src_dir+"/thirdparty/protobuf/protobuf")
+        command_executor(["git", "checkout", "52b244724"])
+        os.chdir(pwd)
+        
 
         build_openvino(build_dir, openvino_src_dir, cxx_abi, target_arch,
                        artifacts_location, arguments.debug_build, verbosity)
