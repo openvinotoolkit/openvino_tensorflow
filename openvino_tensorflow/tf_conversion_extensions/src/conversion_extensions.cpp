b1d0aab1c7677e5e87d2cde7279349494d8f0d33
kotreshr <111343878+kotreshr@users.noreply.github.com>
2022-08-30 04:38:37 +0530

    Kotreshr/ctc greedy decoder conversion extension added (#360)
    * ctc_greedy_decoder tf_conversion_extension added
    
    * custom tf conversion extension for sparse to dense added
    
    * conv2d_kernels_check unit test modified as per the exception info changes

--- openvino_tensorflow/tf_conversion_extensions/src/conversion_extensions.cpp
+++ openvino_tensorflow/tf_conversion_extensions/src/conversion_extensions.cpp
@@ -55,4 +55,10 @@ OPENVINO_CREATE_EXTENSIONS(std::vector<ov::Extension::Ptr>({
     std::make_shared<ov::frontend::tensorflow::ConversionExtension>(
         "Cast",
         ov::frontend::tensorflow::op::translate_cast_op),
+    std::make_shared<ov::frontend::tensorflow::ConversionExtension>(
+        "CTCGreedyDecoder",
+        ov::frontend::tensorflow::op::translate_ctc_greedy_decoder_op),
+    std::make_shared<ov::frontend::tensorflow::ConversionExtension>(
+        "SparseToDense",
+        ov::frontend::tensorflow::op::translate_sparse_to_dense_op),
 }));
