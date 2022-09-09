9cdcb5eabf079548002000ab2a11973481f69d19
unknown <intel@BGAOPTIPLEX001.gar.corp.intel.com>
2022-09-09 05:59:40 -0700

    build fixes for windows

--- build_ovtf.py
+++ build_ovtf.py
@@ -367,7 +367,7 @@ def main():
             if tags.interpreter == "cp39":
                 command_executor([
                     "pip", "install", "--force-reinstall",
-                    "https://github.com/openvinotoolkit/openvino_tensorflow/releases/download/v2.1.0/tensorflow-2.9.1-cp39-cp39-win_amd64.whl"
+                    "https://github.com/openvinotoolkit/openvino_tensorflow/releases/download/v2.2.0/tensorflow-2.9.1-cp39-cp39-win_amd64.whl"
                 ])
             else:
                 raise AssertionError("Only python39 is supported on Windows")
