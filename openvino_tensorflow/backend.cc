e28a01d712a7f522fcf65c3467668cbc1c866770
Cavus Mustafa <mustafa.cavus@intel.com>
2022-09-27 17:12:54 -0700

    Added debug prints to backend for troubleshooting

--- openvino_tensorflow/backend.cc
+++ openvino_tensorflow/backend.cc
@@ -16,55 +16,82 @@ namespace openvino_tensorflow {
 static unique_ptr<GlobalContext> g_global_context;
 
 Backend::Backend(const string& config) {
+  std::cout << "OVTF_DEBUG - Backend::Backend - 1" << std::endl;
   string device = config.substr(0, config.find("_"));
+  std::cout << "OVTF_DEBUG - Backend::Backend - 2" << std::endl;
   string prec = "";
+  std::cout << "OVTF_DEBUG - Backend::Backend - 3" << std::endl;
   if (config.find("_") != string::npos)
     prec = config.substr(config.find("_") + 1);
+  std::cout << "OVTF_DEBUG - Backend::Backend - 4" << std::endl;
   ov::Core core;
+  std::cout << "OVTF_DEBUG - Backend::Backend - 5" << std::endl;
   auto devices = core.get_available_devices();
+  std::cout << "OVTF_DEBUG - Backend::Backend - 6" << std::endl;
   // TODO: Handle multiple devices
 
   bool dev_found = false;
+  std::cout << "OVTF_DEBUG - Backend::Backend - 7" << std::endl;
   if (find(devices.begin(), devices.end(), device) == devices.end()) {
+  std::cout << "OVTF_DEBUG - Backend::Backend - 8" << std::endl;
     if (device == "MYRIAD") {
+  std::cout << "OVTF_DEBUG - Backend::Backend - 9" << std::endl;
       for (auto dev : devices) {
+  std::cout << "OVTF_DEBUG - Backend::Backend - 10" << std::endl;
         if (dev.find(device) != std::string::npos) dev_found = true;
+  std::cout << "OVTF_DEBUG - Backend::Backend - 11" << std::endl;
       }
     }
   } else {
+  std::cout << "OVTF_DEBUG - Backend::Backend - 12" << std::endl;
     dev_found = true;
+  std::cout << "OVTF_DEBUG - Backend::Backend - 13" << std::endl;
   }
 
+  std::cout << "OVTF_DEBUG - Backend::Backend - 14" << std::endl;
   if (!dev_found) {
+  std::cout << "OVTF_DEBUG - Backend::Backend - 15" << std::endl;
     stringstream ss;
     ss << "Device '" << config << "' not found.";
     throw runtime_error(ss.str());
   }
 
+  std::cout << "OVTF_DEBUG - Backend::Backend - 16" << std::endl;
   if ((device.find("GPU") != std::string::npos) && prec != "" &&
       prec != "FP16") {  // device == GPU
+  std::cout << "OVTF_DEBUG - Backend::Backend - 17" << std::endl;
     stringstream ss;
+  std::cout << "OVTF_DEBUG - Backend::Backend - 18" << std::endl;
     if (prec == "FP32") {
+  std::cout << "OVTF_DEBUG - Backend::Backend - 19" << std::endl;
       ss << "'GPU_FP32' is not a supported device name."
          << " Please use 'GPU' device name for FP32 precision.";
       throw runtime_error(ss.str());
     } else {
+  std::cout << "OVTF_DEBUG - Backend::Backend - 20" << std::endl;
       ss << "The precision '" << prec << "' is not supported on 'GPU'.";
       throw runtime_error(ss.str());
     }
+  std::cout << "OVTF_DEBUG - Backend::Backend - 21" << std::endl;
   } else if ((device.find("GPU") == std::string::npos) &&
              prec != "") {  // device != GPU
+  std::cout << "OVTF_DEBUG - Backend::Backend - 22" << std::endl;
     stringstream ss;
     ss << "Device '" << device << "' does not support custom precisions.";
     throw runtime_error(ss.str());
   }
 
+  std::cout << "OVTF_DEBUG - Backend::Backend - 23" << std::endl;
   m_device_type = config;
+  std::cout << "OVTF_DEBUG - Backend::Backend - 24" << std::endl;
   if (config.find("MYRIAD") != std::string::npos) {
+  std::cout << "OVTF_DEBUG - Backend::Backend - 25" << std::endl;
     m_device = "MYRIAD";
   } else {
+  std::cout << "OVTF_DEBUG - Backend::Backend - 26" << std::endl;
     m_device = device;
   }
+  std::cout << "OVTF_DEBUG - Backend::Backend - 27" << std::endl;
 }
 
 shared_ptr<Executable> Backend::Compile(shared_ptr<ov::Model> func, bool) {
--- openvino_tensorflow/backend_manager.cc
+++ openvino_tensorflow/backend_manager.cc
@@ -26,19 +26,30 @@ BackendManager::~BackendManager() {
 
 Status BackendManager::SetBackend(const string& backend_name) {
   OVTF_VLOG(2) << "BackendManager::SetBackend(" << backend_name << ")";
+  std::cout << "OVTF_DEBUG - SetBackend - 1" << std::endl;
   shared_ptr<Backend> backend;
+  std::cout << "OVTF_DEBUG - SetBackend - 2" << std::endl;
   string bname(backend_name);
+  std::cout << "OVTF_DEBUG - SetBackend - 3" << std::endl;
 
   auto status = CreateBackend(backend, bname);
+  std::cout << "OVTF_DEBUG - SetBackend - 4" << std::endl;
   if (!status.ok() || backend == nullptr) {
+  std::cout << "OVTF_DEBUG - SetBackend - 5" << std::endl;
     return errors::Internal("Failed to set backend: ", status.error_message());
   }
+  std::cout << "OVTF_DEBUG - SetBackend - 6" << std::endl;
 
   lock_guard<mutex> lock(m_backend_mutex);
+  std::cout << "OVTF_DEBUG - SetBackend - 7" << std::endl;
   m_backend = backend;
+  std::cout << "OVTF_DEBUG - SetBackend - 8" << std::endl;
   if (bname.find("MYRIAD") != string::npos) {
+  std::cout << "OVTF_DEBUG - SetBackend - 9" << std::endl;
     m_backend_name = "MYRIAD";
+  std::cout << "OVTF_DEBUG - SetBackend - 10" << std::endl;
     m_tf_frontend_disabled = true;
+  std::cout << "OVTF_DEBUG - SetBackend - 11" << std::endl;
   } else if (bname.find("GPU") != string::npos) {
     // Since m_backend_name is assigned "GPU" whenever the string "GPU" is found
     // in bname,
@@ -48,39 +59,61 @@ Status BackendManager::SetBackend(const string& backend_name) {
     // support.
     // In OVTF, we will assume that all types of GPU devices have the same Op
     // support.
+  std::cout << "OVTF_DEBUG - SetBackend - 12" << std::endl;
     m_backend_name = "GPU";
+  std::cout << "OVTF_DEBUG - SetBackend - 13" << std::endl;
   } else {
+  std::cout << "OVTF_DEBUG - SetBackend - 14" << std::endl;
     m_backend_name = bname;
+  std::cout << "OVTF_DEBUG - SetBackend - 15" << std::endl;
     if (bname.find("HDDL") != string::npos) m_tf_frontend_disabled = true;
+  std::cout << "OVTF_DEBUG - SetBackend - 16" << std::endl;
   }
+  std::cout << "OVTF_DEBUG - SetBackend - 17" << std::endl;
   // read value of OPENVINO_TF_ENABLE_PERF_COUNT
   const char* openvino_tf_enable_perf_count =
       std::getenv("OPENVINO_TF_ENABLE_PERF_COUNT");
+  std::cout << "OVTF_DEBUG - SetBackend - 18" << std::endl;
   if (openvino_tf_enable_perf_count != nullptr) {
+  std::cout << "OVTF_DEBUG - SetBackend - 19" << std::endl;
     if (1 == std::stoi(openvino_tf_enable_perf_count)) {
+  std::cout << "OVTF_DEBUG - SetBackend - 20" << std::endl;
       m_perf_counters_enabled = true;
+  std::cout << "OVTF_DEBUG - SetBackend - 21" << std::endl;
     }
   }
+  std::cout << "OVTF_DEBUG - SetBackend - 22" << std::endl;
 
   // read value of OPENVINO_TF_ENABLE_OVTF_PROFILING
   const char* openvino_tf_enable_ovtf_profiling =
       std::getenv("OPENVINO_TF_ENABLE_OVTF_PROFILING");
+  std::cout << "OVTF_DEBUG - SetBackend - 23" << std::endl;
   if (openvino_tf_enable_ovtf_profiling != nullptr) {
+  std::cout << "OVTF_DEBUG - SetBackend - 24" << std::endl;
     if (1 == std::stoi(openvino_tf_enable_ovtf_profiling)) {
+  std::cout << "OVTF_DEBUG - SetBackend - 25" << std::endl;
       m_enable_ovtf_profiling = true;
+  std::cout << "OVTF_DEBUG - SetBackend - 26" << std::endl;
     }
   }
+  std::cout << "OVTF_DEBUG - SetBackend - 27" << std::endl;
 
   const char* openvino_tf_disable_tffe =
       std::getenv("OPENVINO_TF_DISABLE_TFFE");
+  std::cout << "OVTF_DEBUG - SetBackend - 28" << std::endl;
   if (openvino_tf_disable_tffe != nullptr) {
+  std::cout << "OVTF_DEBUG - SetBackend - 29" << std::endl;
     if (1 == std::stoi(openvino_tf_disable_tffe)) {
+  std::cout << "OVTF_DEBUG - SetBackend - 30" << std::endl;
       m_tf_frontend_disabled = true;
+  std::cout << "OVTF_DEBUG - SetBackend - 31" << std::endl;
     }
   }
 
+  std::cout << "OVTF_DEBUG - SetBackend - 32" << std::endl;
   //  read value of OPENVINO_TF_MODEL_CACHE_DIR
   m_model_cache_dir = std::getenv("OPENVINO_TF_MODEL_CACHE_DIR");
+  std::cout << "OVTF_DEBUG - SetBackend - 33" << std::endl;
 
   return Status::OK();
 }
@@ -115,32 +148,48 @@ Status BackendManager::GetBackendName(string& backend_name) {
 
 Status BackendManager::CreateBackend(shared_ptr<Backend>& backend,
                                      string& backend_name) {
+  std::cout << "OVTF_DEBUG - CreateBackend - 1" << std::endl;
   const char* env = std::getenv("OPENVINO_TF_BACKEND");
   // Array should be of max length MYRIAD.
+  std::cout << "OVTF_DEBUG - CreateBackend - 2" << std::endl;
   char backendName[7];
 
+  std::cout << "OVTF_DEBUG - CreateBackend - 3" << std::endl;
   if (env != nullptr) {
+  std::cout << "OVTF_DEBUG - CreateBackend - 4" << std::endl;
     strncpy((char*)backendName, env, sizeof(backendName));
+  std::cout << "OVTF_DEBUG - CreateBackend - 5" << std::endl;
     backendName[6] = '\0';  // null terminate to remove warnings
+  std::cout << "OVTF_DEBUG - CreateBackend - 6" << std::endl;
     backend_name = std::string(backendName);
+  std::cout << "OVTF_DEBUG - CreateBackend - 7" << std::endl;
   }
 
+  std::cout << "OVTF_DEBUG - CreateBackend - 8" << std::endl;
   if (backend_name == "HDDL") {
+  std::cout << "OVTF_DEBUG - CreateBackend - 9" << std::endl;
     return errors::Internal("Failed to Create backend: ",
                             backend_name + " backend not available");
   }
+  std::cout << "OVTF_DEBUG - CreateBackend - 10" << std::endl;
   if (backend_name == "VAD-M") backend_name = "HDDL";
+  std::cout << "OVTF_DEBUG - CreateBackend - 11" << std::endl;
 
   try {
+  std::cout << "OVTF_DEBUG - CreateBackend - 12" << std::endl;
     backend = make_shared<Backend>(backend_name);
+  std::cout << "OVTF_DEBUG - CreateBackend - 13" << std::endl;
   } catch (const std::exception& e) {
     return errors::Internal("Could not create backend of type ", backend_name,
                             ". Got exception: ", e.what());
   }
+  std::cout << "OVTF_DEBUG - CreateBackend - 14" << std::endl;
   if (backend == nullptr) {
+  std::cout << "OVTF_DEBUG - CreateBackend - 15" << std::endl;
     return errors::Internal("Could not create backend of type ", backend_name,
                             " got nullptr");
   }
+  std::cout << "OVTF_DEBUG - CreateBackend - 16" << std::endl;
 
   OVTF_VLOG(2) << "BackendManager::CreateBackend(): " << backend_name;
   return Status::OK();
